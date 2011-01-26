aa1753ae7a06525be4179efe90eb8b58182d2c39
Jinpil Lee <jinpil@hpcs.cs.tsukuba.ac.jp>
2011-01-26 13:30:11 +0000

    modify: not use temp buffer for reduction

--- libxmp/src/xmp_reduce.c
+++ libxmp/src/xmp_reduce.c
@@ -22,45 +22,45 @@ static void _XMP_setup_reduce_type(MPI_Datatype *mpi_datatype, size_t *datatype_
 
   switch (datatype) {
 //  case _XMP_N_TYPE_BOOL:
-//    { *mpi_datatype = MPI_C_BOOL;				*datatype_size = sizeof(_Bool); }			break;
+//    { *mpi_datatype = MPI_C_BOOL;			*datatype_size = sizeof(_Bool); 			break; }
     case _XMP_N_TYPE_CHAR:
-      { *mpi_datatype = MPI_SIGNED_CHAR;			*datatype_size = sizeof(char); }			break;
+      { *mpi_datatype = MPI_SIGNED_CHAR;		*datatype_size = sizeof(char); 				break; }
     case _XMP_N_TYPE_UNSIGNED_CHAR:
-      { *mpi_datatype = MPI_UNSIGNED_CHAR;			*datatype_size = sizeof(unsigned char); }		break;
+      { *mpi_datatype = MPI_UNSIGNED_CHAR;		*datatype_size = sizeof(unsigned char); 		break; }
     case _XMP_N_TYPE_SHORT:
-      { *mpi_datatype = MPI_SHORT;				*datatype_size = sizeof(short); }			break;
+      { *mpi_datatype = MPI_SHORT;			*datatype_size = sizeof(short); 			break; }
     case _XMP_N_TYPE_UNSIGNED_SHORT:
-      { *mpi_datatype = MPI_UNSIGNED_SHORT;			*datatype_size = sizeof(unsigned short); }		break;
+      { *mpi_datatype = MPI_UNSIGNED_SHORT;		*datatype_size = sizeof(unsigned short); 		break; }
     case _XMP_N_TYPE_INT:
-      { *mpi_datatype = MPI_INT;				*datatype_size = sizeof(int); }				break;
+      { *mpi_datatype = MPI_INT;			*datatype_size = sizeof(int); 				break; }
     case _XMP_N_TYPE_UNSIGNED_INT:
-      { *mpi_datatype = MPI_UNSIGNED;				*datatype_size = sizeof(unsigned int); }		break;
+      { *mpi_datatype = MPI_UNSIGNED;			*datatype_size = sizeof(unsigned int); 			break; }
     case _XMP_N_TYPE_LONG:
-      { *mpi_datatype = MPI_LONG;				*datatype_size = sizeof(long); }			break;
+      { *mpi_datatype = MPI_LONG;			*datatype_size = sizeof(long); 				break; }
     case _XMP_N_TYPE_UNSIGNED_LONG:
-      { *mpi_datatype = MPI_UNSIGNED_LONG;			*datatype_size = sizeof(unsigned long); }		break;
+      { *mpi_datatype = MPI_UNSIGNED_LONG;		*datatype_size = sizeof(unsigned long); 		break; }
     case _XMP_N_TYPE_LONGLONG:
-      { *mpi_datatype = MPI_LONG_LONG;				*datatype_size = sizeof(long long); }			break;
+      { *mpi_datatype = MPI_LONG_LONG;			*datatype_size = sizeof(long long); 			break; }
     case _XMP_N_TYPE_UNSIGNED_LONGLONG:
-      { *mpi_datatype = MPI_UNSIGNED_LONG_LONG;			*datatype_size = sizeof(unsigned long long); }		break;
+      { *mpi_datatype = MPI_UNSIGNED_LONG_LONG;		*datatype_size = sizeof(unsigned long long); 		break; }
     case _XMP_N_TYPE_FLOAT:
-      { *mpi_datatype = MPI_FLOAT;				*datatype_size = sizeof(float); }			break;
+      { *mpi_datatype = MPI_FLOAT;			*datatype_size = sizeof(float); 			break; }
     case _XMP_N_TYPE_DOUBLE:
-      { *mpi_datatype = MPI_DOUBLE;				*datatype_size = sizeof(double); }			break;
+      { *mpi_datatype = MPI_DOUBLE;			*datatype_size = sizeof(double); 			break; }
     case _XMP_N_TYPE_LONG_DOUBLE:
-      { *mpi_datatype = MPI_LONG_DOUBLE;			*datatype_size = sizeof(long double); }			break;
+      { *mpi_datatype = MPI_LONG_DOUBLE;		*datatype_size = sizeof(long double); 			break; }
 //  case _XMP_N_TYPE_FLOAT_IMAGINARY:
-//    { *mpi_datatype = MPI_FLOAT;				*datatype_size = sizeof(float _Imaginary); }		break;
+//    { *mpi_datatype = MPI_FLOAT;			*datatype_size = sizeof(float _Imaginary); 		break; }
 //  case _XMP_N_TYPE_DOUBLE_IMAGINARY:
-//    { *mpi_datatype = MPI_DOUBLE;				*datatype_size = sizeof(double _Imaginary); }		break;
+//    { *mpi_datatype = MPI_DOUBLE;			*datatype_size = sizeof(double _Imaginary); 		break; }
 //  case _XMP_N_TYPE_LONG_DOUBLE_IMAGINARY:
-//    { *mpi_datatype = MPI_LONG_DOUBLE;			*datatype_size = sizeof(long double _Imaginary); }	break;
+//    { *mpi_datatype = MPI_LONG_DOUBLE;		*datatype_size = sizeof(long double _Imaginary);	break; }
 //  case _XMP_N_TYPE_FLOAT_COMPLEX:
-//    { *mpi_datatype = MPI_C_FLOAT_COMPLEX;			*datatype_size = sizeof(float _Complex); }		break;
+//    { *mpi_datatype = MPI_C_FLOAT_COMPLEX;		*datatype_size = sizeof(float _Complex); 		break; }
 //  case _XMP_N_TYPE_DOUBLE_COMPLEX:
-//    { *mpi_datatype = MPI_C_DOUBLE_COMPLEX;			*datatype_size = sizeof(double _Complex); }		break;
+//    { *mpi_datatype = MPI_C_DOUBLE_COMPLEX;		*datatype_size = sizeof(double _Complex); 		break; }
 //  case _XMP_N_TYPE_LONG_DOUBLE_COMPLEX:
-//    { *mpi_datatype = MPI_C_LONG_DOUBLE_COMPLEX;		*datatype_size = sizeof(long double _Complex); }	break;
+//    { *mpi_datatype = MPI_C_LONG_DOUBLE_COMPLEX;	*datatype_size = sizeof(long double _Complex); 		break; }
     default:
       _XMP_fatal("unknown data type for reduction");
   }
@@ -71,33 +71,47 @@ static void _XMP_setup_reduce_op(MPI_Op *mpi_op, int op) {
 
   switch (op) {
     case _XMP_N_REDUCE_SUM:
-      { *mpi_op = MPI_SUM; }	break;
+      *mpi_op = MPI_SUM;
+      break;
     case _XMP_N_REDUCE_PROD:
-      { *mpi_op = MPI_PROD; }	break;
+      *mpi_op = MPI_PROD;
+      break;
     case _XMP_N_REDUCE_BAND:
-      { *mpi_op = MPI_BAND; }	break;
+      *mpi_op = MPI_BAND;
+      break;
     case _XMP_N_REDUCE_LAND:
-      { *mpi_op = MPI_LAND; }	break;
+      *mpi_op = MPI_LAND;
+      break;
     case _XMP_N_REDUCE_BOR:
-      { *mpi_op = MPI_BOR; }	break;
+      *mpi_op = MPI_BOR;
+      break;
     case _XMP_N_REDUCE_LOR:
-      { *mpi_op = MPI_LOR; }	break;
+      *mpi_op = MPI_LOR;
+      break;
     case _XMP_N_REDUCE_BXOR:
-      { *mpi_op = MPI_BXOR; }	break;
+      *mpi_op = MPI_BXOR;
+      break;
     case _XMP_N_REDUCE_LXOR:
-      { *mpi_op = MPI_LXOR; }	break;
+      *mpi_op = MPI_LXOR;
+      break;
     case _XMP_N_REDUCE_MAX:
-      { *mpi_op = MPI_MAX; }	break;
+      *mpi_op = MPI_MAX;
+      break;
     case _XMP_N_REDUCE_MIN:
-      { *mpi_op = MPI_MIN; }	break;
+      *mpi_op = MPI_MIN;
+      break;
     case _XMP_N_REDUCE_FIRSTMAX:
-      { *mpi_op = MPI_MAX; }	break;
+      *mpi_op = MPI_MAX;
+      break;
     case _XMP_N_REDUCE_FIRSTMIN:
-      { *mpi_op = MPI_MIN; }	break;
+      *mpi_op = MPI_MIN;
+      break;
     case _XMP_N_REDUCE_LASTMAX:
-      { *mpi_op = MPI_MAX; }	break;
+      *mpi_op = MPI_MAX;
+      break;
     case _XMP_N_REDUCE_LASTMIN:
-      { *mpi_op = MPI_MIN; }	break;
+      *mpi_op = MPI_MIN;
+      break;
     default:
       _XMP_fatal("unknown reduce operation");
   }
@@ -109,10 +123,12 @@ static void _XMP_setup_reduce_FLMM_op(MPI_Op *mpi_op, int op) {
   switch (op) {
     case _XMP_N_REDUCE_FIRSTMAX:
     case _XMP_N_REDUCE_FIRSTMIN:
-      { *mpi_op = MPI_MIN; } break;
+      *mpi_op = MPI_MIN;
+      break;
     case _XMP_N_REDUCE_LASTMAX:
     case _XMP_N_REDUCE_LASTMIN:
-      { *mpi_op = MPI_MAX; } break;
+      *mpi_op = MPI_MAX;
+      break;
     default:
       _XMP_fatal("unknown reduce operation");
   }
@@ -126,7 +142,8 @@ static void _XMP_setup_reduce_FLMM_op(MPI_Op *mpi_op, int op) {
     if (buf1[i] == buf2[i]) cmp_buffer[i] = true; \
     else                    cmp_buffer[i] = false; \
   } \
-} break;
+  break; \
+}
 
 static void _XMP_compare_reduce_results(_Bool *cmp_buffer, void *temp_buffer, void *addr, int count, int datatype) {
   _XMP_ASSERT(cmp_buffer != NULL);
@@ -134,20 +151,20 @@ static void _XMP_compare_reduce_results(_Bool *cmp_buffer, void *temp_buffer, vo
   _XMP_ASSERT(addr != NULL);
 
   switch (datatype) {
-    case _XMP_N_TYPE_BOOL:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(_Bool)
-    case _XMP_N_TYPE_CHAR:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(char)
-    case _XMP_N_TYPE_UNSIGNED_CHAR:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned char)
-    case _XMP_N_TYPE_SHORT:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(short)
-    case _XMP_N_TYPE_UNSIGNED_SHORT:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned short)
-    case _XMP_N_TYPE_INT:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(int)
-    case _XMP_N_TYPE_UNSIGNED_INT:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned int)
-    case _XMP_N_TYPE_LONG:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(long)
-    case _XMP_N_TYPE_UNSIGNED_LONG:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned long)
-    case _XMP_N_TYPE_LONGLONG:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(long long)
-    case _XMP_N_TYPE_UNSIGNED_LONGLONG:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned long long)
-    case _XMP_N_TYPE_FLOAT:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(float)
-    case _XMP_N_TYPE_DOUBLE:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(double)
-    case _XMP_N_TYPE_LONG_DOUBLE:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(long double)
+    case _XMP_N_TYPE_BOOL:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(_Bool);
+    case _XMP_N_TYPE_CHAR:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(char);
+    case _XMP_N_TYPE_UNSIGNED_CHAR:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned char);
+    case _XMP_N_TYPE_SHORT:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(short);
+    case _XMP_N_TYPE_UNSIGNED_SHORT:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned short);
+    case _XMP_N_TYPE_INT:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(int);
+    case _XMP_N_TYPE_UNSIGNED_INT:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned int);
+    case _XMP_N_TYPE_LONG:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(long);
+    case _XMP_N_TYPE_UNSIGNED_LONG:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned long);
+    case _XMP_N_TYPE_LONGLONG:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(long long);
+    case _XMP_N_TYPE_UNSIGNED_LONGLONG:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(unsigned long long);
+    case _XMP_N_TYPE_FLOAT:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(float);
+    case _XMP_N_TYPE_DOUBLE:			_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(double);
+    case _XMP_N_TYPE_LONG_DOUBLE:		_XMP_M_COMPARE_REDUCE_RESULTS_MAIN(long double);
     case _XMP_N_TYPE_FLOAT_IMAGINARY:
     case _XMP_N_TYPE_DOUBLE_IMAGINARY:
     case _XMP_N_TYPE_LONG_DOUBLE_IMAGINARY:
@@ -169,32 +186,35 @@ static void _XMP_compare_reduce_results(_Bool *cmp_buffer, void *temp_buffer, vo
       switch (op) { \
         case _XMP_N_REDUCE_FIRSTMAX: \
         case _XMP_N_REDUCE_FIRSTMIN: \
-          { buf[i] = init_max; break; } \
+          buf[i] = init_max; \
+          break; \
         case _XMP_N_REDUCE_LASTMAX: \
         case _XMP_N_REDUCE_LASTMIN: \
-          { buf[i] = init_min; break; } \
+          buf[i] = init_min; \
+          break; \
         default: \
           _XMP_fatal("unknown reduce operation"); \
       } \
     } \
   } \
-} break;
+  break; \
+}
 
 static void _XMP_init_localtion_variables(void *loc, int count, int loc_datatype, _Bool *cmp_buffer, int op) {
   _XMP_ASSERT(loc != NULL);
   _XMP_ASSERT(cmp_buffer != NULL);
 
   switch (loc_datatype) {
-    case _XMP_N_TYPE_CHAR:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(char, SCHAR_MIN, SCHAR_MAX)
-    case _XMP_N_TYPE_UNSIGNED_CHAR:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned char, 0, UCHAR_MAX)
-    case _XMP_N_TYPE_SHORT:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(short, SHRT_MIN, SHRT_MAX)
-    case _XMP_N_TYPE_UNSIGNED_SHORT:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned short, 0, USHRT_MAX)
-    case _XMP_N_TYPE_INT:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(int, INT_MIN, INT_MAX)
-    case _XMP_N_TYPE_UNSIGNED_INT:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned int, 0, UINT_MAX)
-    case _XMP_N_TYPE_LONG:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(long, LONG_MIN, LONG_MAX)
-    case _XMP_N_TYPE_UNSIGNED_LONG:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned long, 0, ULONG_MAX)
-    case _XMP_N_TYPE_LONGLONG:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(long long, LLONG_MIN, LLONG_MAX)
-    case _XMP_N_TYPE_UNSIGNED_LONGLONG:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned long long, 0, ULLONG_MAX)
+    case _XMP_N_TYPE_CHAR:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(char, SCHAR_MIN, SCHAR_MAX);
+    case _XMP_N_TYPE_UNSIGNED_CHAR:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned char, 0, UCHAR_MAX);
+    case _XMP_N_TYPE_SHORT:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(short, SHRT_MIN, SHRT_MAX);
+    case _XMP_N_TYPE_UNSIGNED_SHORT:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned short, 0, USHRT_MAX);
+    case _XMP_N_TYPE_INT:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(int, INT_MIN, INT_MAX);
+    case _XMP_N_TYPE_UNSIGNED_INT:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned int, 0, UINT_MAX);
+    case _XMP_N_TYPE_LONG:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(long, LONG_MIN, LONG_MAX);
+    case _XMP_N_TYPE_UNSIGNED_LONG:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned long, 0, ULONG_MAX);
+    case _XMP_N_TYPE_LONGLONG:		_XMP_M_INIT_LOCATION_VARIABLES_MAIN(long long, LLONG_MIN, LLONG_MAX);
+    case _XMP_N_TYPE_UNSIGNED_LONGLONG:	_XMP_M_INIT_LOCATION_VARIABLES_MAIN(unsigned long long, 0, ULLONG_MAX);
     default:
       _XMP_fatal("wrong data type for <location-variables>");
   }
@@ -204,7 +224,9 @@ void _XMP_reduce_NODES_ENTIRE(_XMP_nodes_t *nodes, void *addr, int count, int da
   _XMP_ASSERT(nodes != NULL);
   _XMP_ASSERT(addr != NULL);
 
-  if (count == 0) return; // FIXME not good implementation
+  if (count == 0) {
+    return; // FIXME not good implementation
+  }
 
   if (!nodes->is_member) {
     return;
@@ -218,24 +240,20 @@ void _XMP_reduce_NODES_ENTIRE(_XMP_nodes_t *nodes, void *addr, int count, int da
   _XMP_setup_reduce_op(&mpi_op, op);
 
   // reduce
-  size_t n = datatype_size * count;
-  void *temp_buffer = _XMP_alloc(n);
-  memcpy(temp_buffer, addr, n);
-
-  MPI_Allreduce(temp_buffer, addr, count, mpi_datatype, mpi_op, *(nodes->comm));
-
-  _XMP_free(temp_buffer);
+  MPI_Allreduce(MPI_IN_PLACE, addr, count, mpi_datatype, mpi_op, *(nodes->comm));
 }
 
 // _XMP_M_REDUCE_EXEC(addr, count, datatype, op) is in xmp_comm_macro.h
 
 void _XMP_reduce_FLMM_NODES_ENTIRE(_XMP_nodes_t *nodes,
-                                          void *addr, int count, int datatype, int op,
-                                          int num_locs, ...) {
+                                   void *addr, int count, int datatype, int op,
+                                   int num_locs, ...) {
   _XMP_ASSERT(nodes != NULL);
   _XMP_ASSERT(addr != NULL);
 
-  if (count == 0) return; // FIXME not good implementation
+  if (count == 0) {
+    return; // FIXME not good implementation
+  }
 
   if (!nodes->is_member) {
     return;
