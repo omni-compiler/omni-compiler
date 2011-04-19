3943efca87e832c3b40ea002fe99d2c77d6ca937
Jinpil Lee <jinpil@hpcs.cs.tsukuba.ac.jp>
2011-04-19 04:06:21 +0000

    bugfix: link GPGPU library correctly

--- libxmp/src_gpu/xmp_gpu_runtime.cu
+++ libxmp/src_gpu/xmp_gpu_runtime.cu
@@ -1,7 +1,23 @@
+#include <stdio.h>
+
 int _XMP_gpu_device_count;
 
 extern "C" void _XMP_gpu_init(void) {
   cudaGetDeviceCount(&_XMP_gpu_device_count);
+
+  if (_XMP_gpu_device_count == 0) {
+    printf("no device\n");
+    return;
+  }
+
+  for (int i = 0; i < _XMP_gpu_device_count; i++) {
+    cudaDeviceProp dev_prop;
+    cudaGetDeviceProperties(&dev_prop, i);
+
+    printf("device[%d]\n", i);
+    printf(" name = %s\n", dev_prop.name);
+    printf(" CC = %d.%d \n", dev_prop.major, dev_prop.minor);
+  }
 }
 
 extern "C" void _XMP_gpu_finalize(void) {
