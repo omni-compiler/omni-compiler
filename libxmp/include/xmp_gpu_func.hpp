9e14b93cd166b4ab94b6f490a6d365b864ce4a67
Jinpil Lee <jinpil@hpcs.cs.tsukuba.ac.jp>
2011-05-09 03:27:06 +0000

    calc config params for GPU

--- libxmp/include/xmp_gpu_func.hpp
+++ libxmp/include/xmp_gpu_func.hpp
@@ -19,6 +19,14 @@
 #define _XMP_M_MAX(a_, b_) ((a_) > (b_) ? (a_) : (b_))
 #define _XMP_M_MIN(a_, b_) ((a_) > (b_) ? (b_) : (a_))
 
+extern "C" void _XMP_fatal(char *msg);
+
+extern int _XMP_gpu_max_thread;
+
+extern int _XMP_gpu_max_block_dim_x;
+extern int _XMP_gpu_max_block_dim_y;
+extern int _XMP_gpu_max_block_dim_z;
+
 template<typename T>
 __device__ void _XMP_gpu_calc_thread_id(T *index) {
   *index = threadIdx.x +
@@ -33,12 +41,44 @@ template<typename T>
 void _XMP_gpu_calc_config_params(int *block_x, int *block_y, int *block_z,
                                  int *thread_x, int *thread_y, int *thread_z,
                                  T lower, T upper, T stride) {
-  *block_x = 1;
-  *block_y = 1;
-  *block_z = 1;
-  *thread_x = _XMP_M_COUNT_TRIPLETi(lower, upper, stride);
-  *thread_y = 1;
+  unsigned long long num_elmts = _XMP_M_COUNT_TRIPLETi(lower, upper, stride);
+
+  *thread_x = 16;
+  *thread_y = 16;
   *thread_z = 1;
+
+  if ((16 * 16) >= num_elmts) {
+    *block_x = 1;
+    *block_y = 1;
+    *block_z = 1;
+    return;
+  }
+
+  num_elmts = _XMP_M_CEILi(num_elmts, (16 * 16));
+
+  // calc block_x, block_y, block_z
+  if (num_elmts > _XMP_gpu_max_block_dim_x) {
+    *block_x = _XMP_gpu_max_block_dim_x;
+
+    num_elmts = _XMP_M_CEILi(num_elmts, _XMP_gpu_max_block_dim_x);
+    if (num_elmts > _XMP_gpu_max_block_dim_y) {
+      *block_y = _XMP_gpu_max_block_dim_y;
+
+      num_elmts = _XMP_M_CEILi(num_elmts, _XMP_gpu_max_block_dim_y);
+      if (num_elmts > _XMP_gpu_max_block_dim_z) {
+        _XMP_fatal("data is too big for GPU");
+      } else {
+        *block_z = num_elmts;
+      }
+    } else {
+      *block_y = num_elmts;
+      *block_z = 1;
+    }
+  } else {
+    *block_x = num_elmts;
+    *block_y = 1;
+    *block_z = 1;
+  }
 }
 
 template<typename T>
@@ -46,12 +86,49 @@ void _XMP_gpu_calc_config_params_NUM_THREADS(int *block_x, int *block_y, int *bl
                                              int *thread_x, int *thread_y, int *thread_z,
                                              int thread_x_v, int thread_y_v, int thread_z_v,
                                              T lower, T upper, T stride) {
-  *block_x = 1;
-  *block_y = 1;
-  *block_z = 1;
+  unsigned long long num_elmts = _XMP_M_COUNT_TRIPLETi(lower, upper, stride);
+  unsigned long long num_threads = thread_x_v * thread_y_v * thread_z_v;
+
   *thread_x = thread_x_v;
   *thread_y = thread_y_v;
   *thread_z = thread_z_v;
+
+  if (num_threads > _XMP_gpu_max_thread) {
+    _XMP_fatal("too many threads are requested for GPU");
+  }
+
+  if (num_threads >= num_elmts) {
+    *block_x = 1;
+    *block_y = 1;
+    *block_z = 1;
+    return;
+  }
+
+  num_elmts = _XMP_M_CEILi(num_elmts, num_threads);
+
+  // calc block_x, block_y, block_z
+  if (num_elmts > _XMP_gpu_max_block_dim_x) {
+    *block_x = _XMP_gpu_max_block_dim_x;
+    
+    num_elmts = _XMP_M_CEILi(num_elmts, _XMP_gpu_max_block_dim_x);
+    if (num_elmts > _XMP_gpu_max_block_dim_y) {
+      *block_y = _XMP_gpu_max_block_dim_y;
+
+      num_elmts = _XMP_M_CEILi(num_elmts, _XMP_gpu_max_block_dim_y);
+      if (num_elmts > _XMP_gpu_max_block_dim_z) {
+        _XMP_fatal("data is too big for GPU");
+      } else {
+        *block_z = num_elmts;
+      }
+    } else {
+      *block_y = num_elmts;
+      *block_z = 1;
+    }
+  } else {
+    *block_x = num_elmts;
+    *block_y = 1;
+    *block_z = 1;
+  }
 }
 
 #endif // _XMP_GPU_RUNTIME_FUNC_DECL
--- libxmp/src_gpu/xmp_gpu_runtime.cu
+++ libxmp/src_gpu/xmp_gpu_runtime.cu
@@ -2,12 +2,29 @@
 
 int _XMP_gpu_device_count;
 
+int _XMP_gpu_max_thread;
+
+int _XMP_gpu_max_block_dim_x;
+int _XMP_gpu_max_block_dim_y;
+int _XMP_gpu_max_block_dim_z;
+
 extern "C" void _XMP_gpu_init(void) {
+  // FIXME consider multi-GPU
+
   cudaGetDeviceCount(&_XMP_gpu_device_count);
 
   if (_XMP_gpu_device_count == 0) {
     _XMP_fatal("no GPU device found");
   }
+
+  cudaDeviceProp dev_prop;
+  cudaGetDeviceProperties(&dev_prop, 0);
+
+  _XMP_gpu_max_thread = dev_prop.maxThreadsPerBlock;
+
+  _XMP_gpu_max_block_dim_x = dev_prop.maxGridSize[0];
+  _XMP_gpu_max_block_dim_y = dev_prop.maxGridSize[1];
+  _XMP_gpu_max_block_dim_z = dev_prop.maxGridSize[2];
 }
 
 extern "C" void _XMP_gpu_finalize(void) {
