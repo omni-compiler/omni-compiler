886b2bfa6680f2c72d26374a22c18a5d3f2ea233
Hitoshi Murai <h-murai@riken.jp>
2013-08-13 01:49:05 +0000

    add xmpf_pack_vector.c

--- libxmpf/src/xmpf_array.c
+++ libxmpf/src/xmpf_array.c
@@ -225,7 +225,7 @@ void xmpf_array_get_local_size_off__(_XMP_array_t **a_desc, int *i_dim,
     }
     else {
       *size = ai->ser_upper - ai->ser_lower + 1;
-      *off = ai->ser_lower;
+      *off = ai->ser_lower; // dummy
     }
   }
   else {
---
+++ libxmpf/src/xmpf_pack_vector.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+
+int _xmp_omp_num_procs = 1;
+
+void _XMPF_pack_vector(char * restrict dst, char * restrict src,
+		       int count, int blocklength, int stride){
+
+  if (_xmp_omp_num_procs > 1 && count > 8 * _xmp_omp_num_procs){
+#pragma omp parallel for
+    for (int i = 0; i < count; i++){
+      memcpy(dst + i * blocklength, src + i * stride, blocklength);
+    }
+  }
+  else {
+    for (int i = 0; i < count; i++){
+      memcpy(dst + i * blocklength, src + i * stride, blocklength);
+    }
+  }
+
+}
+
+
+void _XMPF_unpack_vector(char * restrict dst, char * restrict src,
+			 int count, int blocklength, int stride){
+
+  if (_xmp_omp_num_procs > 1 && count > 8 * _xmp_omp_num_procs){
+#pragma omp parallel for
+    for (int i = 0; i < count; i++){
+      memcpy(dst + i * stride, src + i * blocklength, blocklength);
+    }
+  }
+  else {
+    for (int i = 0; i < count; i++){
+      memcpy(dst + i * stride, src + i * blocklength, blocklength);
+    }
+  }
+
+}
+
+
+#include "xmpf_internal.h"
+
+int _xmp_reflect_pack_flag = 0;
+
+void _XMPF_check_reflect_type(void)
+{
+  char *reflect_type = getenv("XMP_REFLECT_TYPE");
+  _xmp_omp_num_procs = omp_get_num_procs();
+
+  if (reflect_type){
+
+    if (strcmp(reflect_type, "REFLECT_NOPACK") == 0){
+      _xmp_reflect_pack_flag = 0;
+      xmpf_dbg_printf("REFLECT_NOPACK\n");
+      return;
+    }
+    else if (strcmp(reflect_type, "REFLECT_PACK") == 0){
+      _xmp_reflect_pack_flag = 1;
+      xmpf_dbg_printf("REFLECT_PACK\n");
+      return;
+    }
+
+  }
+
+  // not specified or a wrong value
+  if (_xmp_omp_num_procs > 1){
+    _xmp_reflect_pack_flag = 1;
+    xmpf_dbg_printf("not specified and REFLECT_PACK\n");
+  }
+  else {
+    _xmp_reflect_pack_flag = 0;
+    xmpf_dbg_printf("not specified and REFLECT_NOPACK\n");
+  }
+
+}
