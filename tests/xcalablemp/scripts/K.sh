5cbbe4ab2cd2958053c01e92fbba72d3c2fe1a72
Masahiro Nakao <masahiro.nakao@riken.jp>
2013-12-26 02:00:49 +0000

    Change position of job script for the K computer

---
+++ tests/xcalablemp/scripts/K.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#PJM --rsc-list "node=4"
+#PJM --rsc-list "elapse=00:05:00"
+#PJM --mpi use-rankdir
+
+. /work/system/Env_base
+
+mpiexec -n 4 ../global-view/shadow-reflect/C/shadow_reflect.x
+mpiexec -n 2 ../local-view/coarray/C/coarray_scalar.x
+mpiexec -n 2 ../local-view/coarray/C/coarray_vector.x
+mpiexec -n 2 ../local-view/coarray/C/coarray_stride.x
+mpiexec -n 4 ../local-view/post-wait/C/post_wait.x
+mpiexec -n 2 ../others/Fortran/module_test
