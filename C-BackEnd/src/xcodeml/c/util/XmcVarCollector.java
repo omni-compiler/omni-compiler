f390c2415d958f5aced1b1d5bc3ff012b27af539
Akihiro Tabuchi <tabuchi@hpcs.cs.tsukuba.ac.jp>
2014-02-19 05:15:28 +0000

    change the method of "collectVars"

--- C-BackEnd/src/xcodeml/c/util/XmcVarCollector.java
+++ C-BackEnd/src/xcodeml/c/util/XmcVarCollector.java
@@ -6,13 +6,8 @@
  */
 package xcodeml.c.util;
 
-import xcodeml.util.*;
-
 import org.w3c.dom.*;
-import javax.xml.xpath.*;
 import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
 
 /**
  * Collector of variable references.
@@ -20,41 +15,25 @@ import java.util.ArrayList;
 public class XmcVarCollector {
     public XmcVarCollector(Set<String> variables) {
         this.variables = variables;
-
-        XPathFactory xpathFactory = XPathFactory.newInstance();
-        XPath xpath = xpathFactory.newXPath();
-
-        String[] xpathExprStrings = {
-            "descendant::Var/text()",
-            "descendant::varAddr/text()",
-            "descendant::funcAddr/text()",
-            "descendant::memberRef/text()",
-        };
-
-        xpathExprs = new ArrayList<XPathExpression>();
-        try {
-            for (String xpathExprStr : xpathExprStrings) {
-                xpathExprs.add(xpath.compile(xpathExprStr));
-            }
-        } catch (XPathExpressionException e) {
-            throw new XmTranslationException(e);
-        }
     }
 
-    public void collectVars(Node node) {
-        try {
-            for (XPathExpression expr : xpathExprs) {
-                Object result = expr.evaluate(node, XPathConstants.NODESET);
-                NodeList nodes = (NodeList) result;
-                for (int i = 0; i < nodes.getLength(); i++) {
-                    variables.add(nodes.item(i).getNodeValue());
-                }
+    public void collectVars(Node n){
+        String nodeName = n.getNodeName();      
+        Boolean isVarNode = nodeName.equals("Var")
+                || nodeName.equals("varAddr")
+                || nodeName.equals("funcAddr")
+                || nodeName.equals("memberRef");
+
+        NodeList cnList = n.getChildNodes();
+        for(int i=0; i<cnList.getLength(); i++){
+            Node cn = cnList.item(i);
+            if(isVarNode && cn.getNodeType() == Node.TEXT_NODE){
+                variables.add(cn.getNodeValue());
+            }else{
+                collectVars(cn);
             }
-        } catch (XPathExpressionException e) {
-            throw new XmTranslationException(node, e);
         }
     }
 
     private Set<String> variables;
-    private List<XPathExpression> xpathExprs;
 }
