b14a544672ffb575fc457a4cfc7667ea7e28a8c9
Akihiro Tabuchi <tabuchi@hpcs.cs.tsukuba.ac.jp>
2014-06-14 18:45:33 +0900

    Add tests of subarray update

---
+++ tests/openacc/update_device_subarray.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+#define I 3
+#define J 5
+#define K 7
+#define L 9
+*/
+#define I 11
+#define J 19
+#define K 23
+#define L 29
+
+int test_update_device(int *a, int i_low, int i_len, int j_low, int j_len, int k_low, int k_len, int l_low, int l_len);
+void get_range(int range[2], int size, int type);
+
+int main()
+{
+  int i,j,k,l;
+  int *a;
+
+  a = (int*)malloc(sizeof(int)*I*J*K*L);
+
+  
+#pragma acc data create(a[0:I*J*K*L])
+  {
+    for(i = 0; i < I; i++){
+      for(j = 0; j < J; j++){
+	for(k = 0; k < K; k++){
+	  for(l = 0; l < L; l++){
+	    a[(((i*J)+j)*K+k)*L+l] = i*11 + j*13 + k*17 + l*19;
+	  }
+	}
+      }
+    }
+
+    //tests
+    for(i=0;i<3;i++){
+      for(j=0;j<3;j++){
+	for(k=0;k<3;k++){
+	  for(l=0;l<3;l++){
+	    int range_i[2],range_j[2],range_k[2],range_l[2];
+	    get_range(range_i, I, i);
+	    get_range(range_j, J, j);
+	    get_range(range_k, K, k);
+	    get_range(range_l, L, l);
+	    if(test_update_device(a, range_i[0], range_i[1], range_j[0], range_j[1], range_k[0], range_k[1], range_l[0], range_l[1]) ){
+	      printf("update device([%d:%d][%d:%d][%d:%d][%d:%d]) failed\n", range_i[0], range_i[1], range_j[0], range_j[1], range_l[0], range_l[1], range_l[0], range_l[1]);
+	      exit(1);
+	    }
+	  }
+	}
+      }
+    }
+  }
+  
+  return 0;
+}
+
+int test_update_device(int *a, int i_low, int i_len, int j_low, int j_len, int k_low, int k_len, int l_low, int l_len)
+{
+  int i,j,k,l;
+  int flag = 0;
+  
+#pragma acc data present(a[0:I][0:J][0:K][0:L])
+  {
+    //set zero
+#pragma acc parallel loop
+    for(i=0;i<I*J*K*L;i++){
+      a[i] = 0;
+    }
+
+#pragma acc update device(a[i_low:i_len][j_low:j_len][k_low:k_len][l_low:l_len])
+
+    //check
+#pragma acc parallel loop collapse(4) reduction(+:flag)
+    for(i = 0; i < I; i++){
+      for(j = 0; j < J; j++){
+	for(k = 0; k < K; k++){
+	  for(l = 0; l < L; l++){
+	    if( (i>=i_low&&i<i_low+i_len) && (j>=j_low&&j<j_low+j_len) && (k>=k_low&&k<k_low+k_len) && (l>=l_low&&l<l_low+l_len) ){
+	      if(a[(((i*J)+j)*K+k)*L+l] != i*11 + j*13 + k*17 + l*19){
+		flag += 1;
+	      }
+	    }else{
+	      if(a[(((i*J)+j)*K+k)*L+l] != 0){
+		flag += 1;
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+
+  return flag;
+}
+
+void get_range(int range[2], int size, int type)
+{
+  switch(type){
+  case 0: //full
+    range[0] = 0;
+    range[1] = size;
+    break;
+  case 1: //len=1
+    range[0] = size/2;
+    range[1] = 1;
+    break;
+  case 2: //part
+    range[0] = 1;
+    range[1] = size - 2;
+    break;
+  default:
+    printf("invalid argument\n");
+  }
+}
