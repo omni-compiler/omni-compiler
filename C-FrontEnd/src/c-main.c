acc864ae2947f28702e710e75b39ed29d19d9b81
Hidetoshi Iwashita <hidetoshi.iwashita@riken.jp>
2014-06-17 18:50:14 +0900

    change c-main.c for use of c-ptree.c

--- C-FrontEnd/src/c-main.c
+++ C-FrontEnd/src/c-main.c
@@ -15,6 +15,12 @@
 #include "c-option.h"
 #include "c-ptree.h"
 
+
+/* display parse tree before making XcodeML */
+static unsigned _SW_DISP_PTREE = 000;        // switch bits for dynamic debugging
+#define _DISP_PTREE_IF(flag, expr, msg)  if ((flag) & _SW_DISP_PTREE) dispParseTree(stderr, expr, msg)
+
+
 /**
  * C_Front main.
  */
@@ -47,7 +53,8 @@ main(int argc, char** argv)
     int convertedFileId = 0;
 
     CExpr *expr = execParse(fpIn);
-    // dispParseTree(stderr, expr, "execParse");        /* display parse tree before making XcodeML */
+    _DISP_PTREE_IF(001, expr, "execParse");
+    //    dispParseTree(stderr, expr, "execParse");
 
     if(s_inFile)
         fclose(fpIn);
@@ -56,7 +63,7 @@ main(int argc, char** argv)
         goto end;
 
     reduceExpr(expr);
-    // dispParseTree(stderr, expr, "reduceExpr");        /* display parse tree before making XcodeML */
+    _DISP_PTREE_IF(002, expr, "reduceExpr");
 
     if(s_hasError)
         goto end;
@@ -65,15 +72,15 @@ main(int argc, char** argv)
         printf("compiling ...\n");
 
     compile(expr);
-    // dispParseTree(stderr, expr, "compile");        /* display parse tree before making XcodeML */
+    _DISP_PTREE_IF(004, expr, "compile");
 
     if(s_hasError)
         goto end;
 
     convertSyntax(expr);
-    // dispParseTree(stderr, expr, "convertSyntax");        /* display parse tree before making XcodeML */
+    _DISP_PTREE_IF(010, expr, "convertSyntax");
     collectTypeDesc(expr);
-    // dispParseTree(stderr, expr, "collectTypeDesc");        /* display parse tree before making XcodeML */
+    _DISP_PTREE_IF(020, expr, "collectTypeDesc");
 
     if(s_hasError)
         goto end;
