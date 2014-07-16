39a209b1fb982318105beb9ce5522172664034f9
Takenori Shimosaka <shimosaka@n49.(none)>
2014-07-16 21:33:04 +0900

    added a offset calculation in the "global to local" function of xmp_gmove.c.

---
+++ tests/xcalablemp/global-view/gmove/Fortran/gmove_lc_1a1t_c_off.f90
@@ -0,0 +1,41 @@
+program tgmove
+include 'xmp_lib.h'
+integer i
+integer,parameter :: n=8
+integer a(n),b(n)
+integer irank
+!$xmp nodes p(*)
+!$xmp template tx(n+3)
+!$xmp distribute tx(cyclic) onto p
+!$xmp align a(i) with tx(i+3)
+!$xmp align b(i) with tx(i+3)
+
+irank=xmp_node_num()
+
+!$xmp loop (i) on tx(i+3)
+do i=1,n
+  a(i)=i
+end do
+
+!$xmp loop (i) on tx(i+3)
+do i=1,n
+  b(i)=0
+end do
+
+!$xmp gmove
+b(2:n)=a(2:n)
+
+ierr=0
+!$xmp loop (i) on tx(i+3)
+do i=2,n
+  ierr=ierr+abs(b(i)-a(i))
+end do
+
+!$xmp reduction (max:ierr)
+irank=xmp_node_num()
+if (irank==1) then
+  print *, 'max error=',ierr
+endif
+!call exit(ierr)
+
+end program tgmove
