621a34206fb397b4a01921ceb999f61ca01f25d1
Takenori Shimosaka <shimosaka@n49.(none)>
2014-07-25 16:03:54 +0900

    add test programs of bug 314 and 315.

---
+++ tests/xcalablemp/global-view/loop/F/315_loop.f90
@@ -0,0 +1,15 @@
+program tgmove
+integer i
+integer,parameter :: n=8
+integer a(n)
+!$xmp nodes p(2)
+!$xmp template tx(n+3)
+!$xmp distribute tx(cyclic(2)) onto p
+!$xmp align a(i) with tx(i+3)
+
+!$xmp loop (i) on tx(i+3)
+do i=1,n
+  a(i)=i
+end do
+
+end program tgmove
