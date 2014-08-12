543117ffbb0d070d68611e2b9d5ec63fbd7c2d7c
Masahiro Nakao <masahiro.nakao@riken.jp>
2014-08-12 18:34:38 +0900

    Add tests for TCA

---
+++ tests/xcalablemp/TCA/C/reflect_acc_2_dup_2.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xmp.h>
+#define N 64
+double a[N][N];
+#pragma xmp nodes p(*)
+#pragma xmp template t(0:N-1)
+#pragma xmp distribute t(block) onto p
+#pragma xmp align a[*][i] with t(i)
+#pragma xmp shadow a[0][1:1]
+#define DUMMY_VAL -999
+#define DUMMY_NODE_ID -999
+int result = 0;
+
+int main(int argc, char **argv)
+{
+  int comm_size = xmp_num_nodes();
+  int node_id   = xmp_node_num();
+
+  for(int i=0;i<N;i++){
+#pragma xmp loop on t(j)
+    for(int j=0;j<N;j++)
+      a[i][j] = node_id;
+  }
+
+  int lo_index = N/comm_size*(node_id-1);
+  int hi_index = N/comm_size*node_id-1;
+  int lo_shadow_index = (node_id != 1)?         lo_index-1 : DUMMY_NODE_ID;
+  int hi_shadow_index = (node_id != comm_size)? hi_index+1 : DUMMY_NODE_ID;
+
+  if(lo_shadow_index != DUMMY_NODE_ID)
+    for(int i=0;i<N;i++)
+      a[i][lo_shadow_index] = node_id;
+
+  if(hi_shadow_index != DUMMY_NODE_ID)
+    for(int i=0;i<N;i++)
+      a[i][hi_shadow_index] = node_id;
+
+#pragma xmp barrier
+#pragma acc data copy(a)
+  {
+#pragma xmp reflect_init (a) acc
+#pragma xmp reflect_do (a) acc
+  }
+
+  for(int i=0;i<N;i++){
+#pragma xmp loop on t(j)
+    for(int j=0;j<N;j++)
+      if(a[i][j] != node_id)
+	result = 1;
+  }
+
+  int lo_node_id = (node_id != 1)?         node_id-1 : DUMMY_NODE_ID;
+  int hi_node_id = (node_id != comm_size)? node_id+1 : DUMMY_NODE_ID;
+
+  if(lo_node_id != DUMMY_NODE_ID)
+    for(int i=0;i<N;i++)
+      if(a[i][lo_shadow_index] != lo_node_id)
+	result = 1;
+
+  if(hi_node_id != DUMMY_NODE_ID)
+    for(int i=0;i<N;i++)
+      if(a[i][hi_shadow_index] != hi_node_id)
+	result = 1;
+
+#pragma xmp reduction(+:result)
+#pragma xmp task on p(1)
+  {
+    if(result == 0){
+      printf("PASS\n");
+    }
+    else{
+      fprintf(stderr, "ERROR\n");
+      exit(1);
+    }
+  }
+
+  return 0;
+}
