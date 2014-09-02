b4c668a878231f27c76c62e04fb421d1ecee5aed
Hidetoshi Iwashita <hidetoshi.iwashita@riken.jp>
2014-09-02 21:31:24 +0900

    added omni_collect_init.in

--- libxmpf/src/xmpf_main.f
+++ libxmpf/src/xmpf_main.f
@@ -1,6 +1,8 @@
 ! main program
       call xmpf_init_all_
       call xmpf_module_init_
+      call xmpf_collect_init
       call xmpf_main
+      call xmpf_collect_finalize
       call xmpf_finalize_all_
       end
