0acccc8e2c41214b0310a6613e951a4e024620eb
Takenori Shimosaka <shimosaka@n49.(none)>
2014-11-13 20:11:54 +0900

    modified xmp_lib.h.

--- libxmpf/include/xmp_lib.h
+++ libxmpf/include/xmp_lib.h
@@ -3,26 +3,35 @@ type xmp_desc
   integer*8 :: desc
 end type xmp_desc
 
-interface
-integer(4) function xmp_get_mpi_comm()
-  end function xmp_get_mpi_comm
-
-integer(4) function xmp_num_nodes()
-  end function xmp_num_nodes
-
-integer(4) function xmp_node_num()
-  end function xmp_node_num
-
-integer(4) function xmp_all_num_nodes()
-  end function xmp_all_num_nodes
-
-integer(4) function xmp_all_node_num()
-  end function xmp_all_node_num
-
-real(8) function xmp_wtime()
-  end function xmp_wtime
-
-real(8) function xmp_wtick()
-  end function xmp_wtick
-
-end interface
+integer(4) xmp_get_mpi_comm
+integer(4) xmp_num_nodes
+integer(4) xmp_node_num
+integer(4) xmp_all_num_nodes
+integer(4) xmp_all_node_num
+real(8) xmp_wtime
+real(8) xmp_wtick
+integer(4) xmp_array_ndims
+integer(4) xmp_array_lbound
+integer(4) xmp_array_ubound
+integer(4) xmp_array_lsize
+integer(4) xmp_array_ushadow
+integer(4) xmp_array_lshadow
+integer(4) xmp_array_lead_dim
+integer(4) xmp_array_gtol
+integer(4) xmp_align_axis
+integer(4) xmp_align_offset
+integer(4) xmp_align_replicated
+integer(4) xmp_align_template
+integer(4) xmp_template_fixed
+integer(4) xmp_template_ndims
+integer(4) xmp_template_lbound
+integer(4) xmp_template_ubound
+integer(4) xmp_dist_format
+integer(4) xmp_dist_blocksize
+integer(4) xmp_dist_gblockmap
+integer(4) xmp_dist_nodes
+integer(4) xmp_dist_axis
+integer(4) xmp_nodes_ndims
+integer(4) xmp_nodes_index
+integer(4) xmp_nodes_size
+integer(4) xmp_nodes_equiv
