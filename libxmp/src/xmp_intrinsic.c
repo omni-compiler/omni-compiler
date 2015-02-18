44cc8344970d3327eb571c0e4ce5e4958a8522a9
Masahiro Nakao <masahiro.nakao@riken.jp>
2015-02-18 21:49:09 +0900

    Fix: 380

--- libxmp/src/xmp_intrinsic.c
+++ libxmp/src/xmp_intrinsic.c
@@ -3357,7 +3357,7 @@ static void xmp_matmul_blockf(_XMP_array_t *x_d, _XMP_array_t *a_d, _XMP_array_t
 
    /* matmul */
    /* TODO: X = A * BT -> DGEMM */
-#ifdef OMNI_TARGET_CPU_KCOMPUTER
+#if defined(OMNI_TARGET_CPU_KCOMPUTER) || defined(_XMP_BLAS_LIB)
    dim0_size = x_d->info[0].local_upper - x_d->info[0].local_lower + 1;
    dim1_size = x_d->info[1].local_upper - x_d->info[1].local_lower + 1;
    k = a_d->info[1].ser_size;
