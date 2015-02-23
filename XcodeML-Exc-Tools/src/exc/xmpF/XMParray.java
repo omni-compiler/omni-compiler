7d8fef8a97355e9b6b486f2b7fa05eec01fc2089
Hitoshi Murai <h-murai@riken.jp>
2015-02-23 17:02:31 +0900

    fix bug 383

--- XcodeML-Exc-Tools/src/exc/xmpF/XMParray.java
+++ XcodeML-Exc-Tools/src/exc/xmpF/XMParray.java
@@ -564,9 +564,12 @@ public class XMParray {
     for(int i = 0; i < dims.size(); i++){
       XMPdimInfo info = dims.elementAt(i);
 
+      Xobject lower = info.getLower();
       Xobject upper = null;
       if (info.getUpper() == null && NthAssumedShape >= 0 && NthAssumedShape < XMP.MAX_ASSUMED_SHAPE){
 	upper = Xcons.FarrayRef(sizeArray.Ref(), Xcons.IntConstant(NthAssumedShape), Xcons.IntConstant(i));
+	upper = Xcons.binaryOp(Xcode.PLUS_EXPR, upper, lower);
+	upper = Xcons.binaryOp(Xcode.MINUS_EXPR, upper, Xcons.IntConstant(1));
       }
       else {
 	upper = info.getUpper();
@@ -574,14 +577,14 @@ public class XMParray {
 
       if(info.isAlignAny()){
 	args = Xcons.List(descId.Ref(),Xcons.IntConstant(i),
-			  info.getLower(), upper,
+			  lower, upper,
 			  Xcons.IntConstant(-1),
 			  Xcons.IntConstant(0));
       } else {
 	Xobject off = info.getAlignSubscriptOffset();
 	if(off == null) off = Xcons.IntConstant(0);
 	args = Xcons.List(descId.Ref(),Xcons.IntConstant(i),
-			  info.getLower(), upper,
+			  lower, upper,
 			  Xcons.IntConstant(info.getAlignSubscriptIndex()),
 			  off);
       }
