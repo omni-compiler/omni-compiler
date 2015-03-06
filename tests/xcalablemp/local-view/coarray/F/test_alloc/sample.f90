012ac9b5ac7f93d32b1da419289b2d09dbcd9653
Hidetoshi Iwashita <hidetoshi.iwashita@riken.jp>
2015-03-06 22:55:20 +0900

    check

---
+++ tests/xcalablemp/local-view/coarray/F/test_alloc/sample.f90
@@ -0,0 +1,17 @@
+subroutine EX1
+  include "xmp_lib.h"
+  real :: V1(10,20)[4,*]
+  complex(8) :: V2[*], z
+  integer n(5)
+!  integer :: V3(10)[4,*]
+!  integer, allocatable :: V3(:)[:,:]
+  integer, allocatable :: V3(:)[4,*]
+
+  V1(1:3,j)[k1,k2] = (/1.0,2.0,3.0/)
+  z = V2[1]**2
+  n(1:5) = V3(2::2)[k1,k2]
+!!  n(1:5) = V3(2:10:2)[k1,k2]
+
+end subroutine EX1
+
+end
