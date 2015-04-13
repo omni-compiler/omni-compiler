8e8366f270bd3c54b1e9fe331cf250120ad45e9b
Masahiro Nakao <masahiro.nakao@riken.jp>
2015-04-13 18:34:50 +0900

    Create coarray stride access for the Fujitsu RDMA

--- libxmp/src/xmp_coarray_fjrdma_utils.c
+++ libxmp/src/xmp_coarray_fjrdma_utils.c
@@ -1,74 +1,71 @@
 #include <stdlib.h>
 #include "xmp_internal.h"
 
-uint64_t* _XMP_calc_raddrs_mput(const uint64_t raddr, const _XMP_array_section_t *dst_info, const int dst_dims, 
-				const size_t elmts)
+void _XMP_set_coarray_addresses(const uint64_t addr, const _XMP_array_section_t *array, const int dims, 
+				const size_t elmts, uint64_t* addrs)
 {
-  uint64_t* raddrs = malloc(sizeof(uint64_t) * elmts);
-  uint64_t tmp_start_offset[dst_dims], tmp_stride_offset[dst_dims], tmp_offset[dst_dims];
+  uint64_t stride_offset[dims], tmp[dims];
 
   // Temporally variables to reduce calculation for offset
-  for(int i=0;i<dst_dims;i++){
-    tmp_start_offset[i]  = dst_info[i].start  * dst_info[i].distance;
-    tmp_stride_offset[i] = dst_info[i].stride * dst_info[i].distance;
-  }
+  for(int i=0;i<dims;i++)
+    stride_offset[i] = array[i].stride * array[i].distance;
  
   int num = 0;
-  switch (dst_dims){
+  switch (dims){
   case 1:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      raddrs[num++] = raddr + tmp_offset[0];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      addrs[num++] = addr + tmp[0];
     }
     break;;
   case 2:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      for(int j=0;j<dst_info[1].length;j++){
-        tmp_offset[1] = tmp_start_offset[1] + j * tmp_stride_offset[1] + tmp_offset[0];
-	raddrs[num++] = raddr + tmp_offset[1];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      for(int j=0;j<array[1].length;j++){
+        tmp[1] = stride_offset[1] * j;
+	addrs[num++] = addr + tmp[0] + tmp[1];
       }
     }
     break;;
   case 3:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      for(int j=0;j<dst_info[1].length;j++){
-        tmp_offset[1] = tmp_start_offset[1] + j * tmp_stride_offset[1] + tmp_offset[0];
-        for(int k=0;k<dst_info[2].length;k++){
-          tmp_offset[2] = tmp_start_offset[2] + k * tmp_stride_offset[2] + tmp_offset[1];
-	  raddrs[num++] = raddr + tmp_offset[2];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      for(int j=0;j<array[1].length;j++){
+        tmp[1] = stride_offset[1] * j;
+	for(int k=0;k<array[2].length;k++){
+	  tmp[2] = stride_offset[2] * k;
+	  addrs[num++] = addr + tmp[0] + tmp[1] + tmp[2];
 	}
       }
     }
     break;;
   case 4:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      for(int j=0;j<dst_info[1].length;j++){
-        tmp_offset[1] = tmp_start_offset[1] + j * tmp_stride_offset[1] + tmp_offset[0];
-        for(int k=0;k<dst_info[2].length;k++){
-          tmp_offset[2] = tmp_start_offset[2] + k * tmp_stride_offset[2] + tmp_offset[1];
-          for(int l=0;l<dst_info[3].length;l++){
-            tmp_offset[3] = tmp_start_offset[3] + l * tmp_stride_offset[3] + tmp_offset[2];
-	    raddrs[num++] = raddr + tmp_offset[3];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      for(int j=0;j<array[1].length;j++){
+        tmp[1] = stride_offset[1] * j;
+	for(int k=0;k<array[2].length;k++){
+          tmp[2] = stride_offset[2] * k;
+	  for(int l=0;l<array[3].length;l++){
+	    tmp[3] = stride_offset[3] * l;
+	    addrs[num++] = addr + tmp[0] + tmp[1] + tmp[2] + tmp[3];
 	  }
-        }
+	}
       }
     }
     break;;
   case 5:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      for(int j=0;j<dst_info[1].length;j++){
-        tmp_offset[1] = tmp_start_offset[1] + j * tmp_stride_offset[1] + tmp_offset[0];
-        for(int k=0;k<dst_info[2].length;k++){
-          tmp_offset[2] = tmp_start_offset[2] + k * tmp_stride_offset[2] + tmp_offset[1];
-          for(int l=0;l<dst_info[3].length;l++){
-            tmp_offset[3] = tmp_start_offset[3] + l * tmp_stride_offset[3] + tmp_offset[2];
-            for(int m=0;m<dst_info[4].length;m++){
-              tmp_offset[4] = tmp_start_offset[4] + m * tmp_stride_offset[4] + tmp_offset[3];
-	      raddrs[num++] = raddr + tmp_offset[4];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      for(int j=0;j<array[1].length;j++){
+        tmp[1] = stride_offset[1] * j;
+        for(int k=0;k<array[2].length;k++){
+          tmp[2] = stride_offset[2] * k;
+          for(int l=0;l<array[3].length;l++){
+            tmp[3] = stride_offset[3] * l;
+	    for(int m=0;m<array[4].length;m++){
+	      tmp[4] = stride_offset[4] * m;
+	      addrs[num++] = addr + tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4];
 	    }
           }
         }
@@ -76,19 +73,19 @@ uint64_t* _XMP_calc_raddrs_mput(const uint64_t raddr, const _XMP_array_section_t
     }
     break;;
   case 6:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      for(int j=0;j<dst_info[1].length;j++){
-        tmp_offset[1] = tmp_start_offset[1] + j * tmp_stride_offset[1] + tmp_offset[0];
-        for(int k=0;k<dst_info[2].length;k++){
-          tmp_offset[2] = tmp_start_offset[2] + k * tmp_stride_offset[2] + tmp_offset[1];
-          for(int l=0;l<dst_info[3].length;l++){
-            tmp_offset[3] = tmp_start_offset[3] + l * tmp_stride_offset[3] + tmp_offset[2];
-            for(int m=0;m<dst_info[4].length;m++){
-              tmp_offset[4] = tmp_start_offset[4] + m * tmp_stride_offset[4] + tmp_offset[3];
-              for(int n=0;n<dst_info[5].length;n++){
-                tmp_offset[5] = tmp_start_offset[5] + n * tmp_stride_offset[5] + tmp_offset[4];
-		raddrs[num++] = raddr + tmp_offset[5];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      for(int j=0;j<array[1].length;j++){
+        tmp[1] = stride_offset[1] * j;
+        for(int k=0;k<array[2].length;k++){
+          tmp[2] = stride_offset[2] * k;
+          for(int l=0;l<array[3].length;l++){
+            tmp[3] = stride_offset[3] * l;
+            for(int m=0;m<array[4].length;m++){
+              tmp[4] = stride_offset[4] * m;
+	      for(int n=0;n<array[5].length;n++){
+		tmp[5] = stride_offset[5] * n;
+		addrs[num++] = addr + tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4] + tmp[5];
 	      }
             }
           }
@@ -97,23 +94,23 @@ uint64_t* _XMP_calc_raddrs_mput(const uint64_t raddr, const _XMP_array_section_t
     }
     break;;
   case 7:
-    for(int i=0;i<dst_info[0].length;i++){
-      tmp_offset[0] = tmp_start_offset[0] + i * tmp_stride_offset[0];
-      for(int j=0;j<dst_info[1].length;j++){
-        tmp_offset[1] = tmp_start_offset[1] + j * tmp_stride_offset[1] + tmp_offset[0];
-        for(int k=0;k<dst_info[2].length;k++){
-          tmp_offset[2] = tmp_start_offset[2] + k * tmp_stride_offset[2] + tmp_offset[1];
-          for(int l=0;l<dst_info[3].length;l++){
-            tmp_offset[3] = tmp_start_offset[3] + l * tmp_stride_offset[3] + tmp_offset[2];
-            for(int m=0;m<dst_info[4].length;m++){
-              tmp_offset[4] = tmp_start_offset[4] + m * tmp_stride_offset[4] + tmp_offset[3];
-              for(int n=0;n<dst_info[5].length;n++){
-                tmp_offset[5] = tmp_start_offset[5] + n * tmp_stride_offset[5] + tmp_offset[4];
-                for(int p=0;p<dst_info[6].length;p++){
-                  tmp_offset[6] = tmp_start_offset[6] + p * tmp_stride_offset[6] + tmp_offset[5];
-		  raddrs[num++] = raddr + tmp_offset[6];
+    for(int i=0;i<array[0].length;i++){
+      tmp[0] = stride_offset[0] * i;
+      for(int j=0;j<array[1].length;j++){
+        tmp[1] = stride_offset[1] * j;
+        for(int k=0;k<array[2].length;k++){
+          tmp[2] = stride_offset[2] * k;
+          for(int l=0;l<array[3].length;l++){
+            tmp[3] = stride_offset[3] * l;
+            for(int m=0;m<array[4].length;m++){
+              tmp[4] = stride_offset[4] * m;
+              for(int n=0;n<array[5].length;n++){
+                tmp[5] = stride_offset[5] * n;
+		for(int p=0;p<array[6].length;p++){
+		  tmp[6] = stride_offset[6] * p;
+		  addrs[num++] = addr + tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4] + tmp[5] + tmp[6];
 		}
-              }
+	      }
             }
           }
         }
@@ -121,26 +118,5 @@ uint64_t* _XMP_calc_raddrs_mput(const uint64_t raddr, const _XMP_array_section_t
     }
     break;;
   }
-
-  return raddrs;
 }
 
-uint64_t* _XMP_calc_laddrs_mput(const uint64_t laddr, const size_t elmts)
-{
-  uint64_t *laddrs = malloc(sizeof(uint64_t) * elmts);
-
-  for(int i=0;i<elmts;i++)
-    laddrs[i] = laddr;
-
-  return laddrs;
-}
-
-size_t* _XMP_calc_lengths_mput(const size_t length, const size_t elmts)
-{
-  size_t *lengths = malloc(sizeof(size_t) * elmts);
-
-  for(int i=0;i<elmts;i++)
-    lengths[i] = length;
-   
-  return lengths;
-}
