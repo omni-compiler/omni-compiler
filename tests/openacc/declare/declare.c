db1388aa124c287187b8838548e3981ed7c702b1
Akihiro Tabuchi <tabuchi@hpcs.cs.tsukuba.ac.jp>
2015-04-27 16:56:23 +0900

    Fix openacc tests

---
+++ tests/openacc/declare/declare.c
@@ -0,0 +1,24 @@
+int a[10];
+double b[100];
+
+#pragma acc declare create(a) copy(b)
+
+int main()
+{
+  int i;
+
+#pragma acc parallel loop present(a)
+  for(i=0;i<10;i++){
+    a[i] = i;
+  }
+
+#pragma acc data present(b)
+  {
+#pragma acc parallel loop
+    for(i=0;i<100;i++){
+      b[i] = i + 1.0;
+    }
+  }
+
+  return 0;
+}
