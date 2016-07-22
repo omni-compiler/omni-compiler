916e1dac0216a22e04b839999c6761b7c8157621
Akihiro Tabuchi <tabuchi@hpcs.cs.tsukuba.ac.jp>
2016-07-22 22:02:48 +0900

    Fix reduction when num_gangs is larger than max_num_gangs

--- libacc/src/acc_util.c
+++ libacc/src/acc_util.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "acc_internal.h"
 #include <signal.h>
+#include <limits.h>
 
 void *_ACC_alloc(size_t size) {
   void *addr;
@@ -25,4 +26,27 @@ void _ACC_fatal(const char *msg) {
 
 void _ACC_unexpected_error(void) {
   _ACC_fatal("unexpected error in runtime");
+}
+
+static inline
+int ceilll(long long a, long long b)
+{
+  return (a-1)/b + 1;
+}
+
+int _ACC_adjust_num_gangs(long long num_gangs, int limit)
+{
+  if(_ACC_num_gangs_limit == 0){
+    limit = INT_MAX;
+  }
+
+  if(_ACC_num_gangs_limit > 0){
+    limit = _ACC_num_gangs_limit;
+  }
+
+  if(num_gangs > limit){
+    return ceilll(num_gangs, ceilll(num_gangs, limit));
+  }else{
+    return (int)num_gangs;
+  }
 }
