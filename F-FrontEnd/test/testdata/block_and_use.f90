c174e1a2c41f63600abe3200f6cc5007e25c1a96
shingo suzuki <shingo-s@sra.co.jp>
2016-07-26 15:35:16 +0900

    Update test data

--- F-FrontEnd/test/testdata/block_and_use.f90
+++ F-FrontEnd/test/testdata/block_and_use.f90
@@ -1,11 +1,14 @@
       MODULE block_and_use
-        INTEGER :: a
+        REAL :: i
       END MODULE block_and_use
 
       PROGRAM MAIN
-        use block_and_use
+        IMPLICIT NONE
         BLOCK
-          use block_and_use
+          USE block_and_use
+          INTEGER :: j
+          i = 2.5
+          j = EXPONENT(i)
         END BLOCK
       END PROGRAM MAIN
 
