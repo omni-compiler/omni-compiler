015f4457c3aa9402d7c88b7ba3b7b9bf62b01cb5
shingo suzuki <shingo-s@sra.co.jp>
2016-08-09 18:34:10 +0900

    Add tests for labels in block-construct

---
+++ F-FrontEnd/test/failtestdata/block_goto_inside_of_block.f90
@@ -0,0 +1,9 @@
+
+      PROGRAM MAIN
+
+      BLOCK
+1000  WRITE (*,*) "hoge"
+      END BLOCK
+      GOTO 1000
+
+      END PROGRAM MAIN
---
+++ F-FrontEnd/test/testdata/block_goto_out_of_block.f90
@@ -0,0 +1,9 @@
+
+      PROGRAM MAIN
+
+      BLOCK
+        GOTO 1000
+      END BLOCK
+1000  WRITE (*,*) "hoge"
+
+      END PROGRAM MAIN
