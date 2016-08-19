850f508fb95fde1f345239c8d594454f1a153756
shingo suzuki <shingo-s@sra.co.jp>
2016-08-19 11:14:51 +0900

    Fix a testdata

--- F-FrontEnd/test/testdata/parameterized_type.f90
+++ F-FrontEnd/test/testdata/parameterized_type.f90
@@ -1,16 +1,16 @@
 
       PROGRAM MAIN
-        TYPE st(dim, lg)
-           INTEGER, KIND :: dim
+        TYPE st(k, lg)
+           INTEGER, KIND :: k
            INTEGER, LEN :: lg
-           REAL :: array(dim)
+           REAL(k) :: r
            CHARACTER(LEN=lg) :: ch
         END TYPE st
 
-        TYPE(st(dim=2,len=4)) :: a
+        TYPE(st(k=2,lg=4)) :: a
 
         TYPE(st):: b
 
-        b = st(dim=2,len=5)(array=(/1,2/), ch="hoge")
+        b = st(k=2,lg=5)(r=1.0, ch="hoge")
 
       END PROGRAM MAIN
