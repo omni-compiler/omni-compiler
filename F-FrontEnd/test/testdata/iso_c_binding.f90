d4b2933b901780857fb9b216538030279d81906e
Valentin Clement <clementval@gmail.com>
2016-11-02 12:08:37 +0100

    Enhance the test data

--- F-FrontEnd/test/testdata/iso_c_binding.f90
+++ F-FrontEnd/test/testdata/iso_c_binding.f90
@@ -4,6 +4,18 @@ type, bind(C) :: ctype
   integer :: len
 end type ctype
 
+interface
+  function send_to_port(port, data, length) bind(C)
+    use, intrinsic :: iso_c_binding, only: c_int, c_char
+    implicit none
+    integer(c_int), value :: port
+    character(kind=c_char) :: data(*)
+    integer(c_int), value :: length
+    integer(c_int) :: send_to_port
+  end function send_to_port
+end interface
+
+
 contains
   function sleep() bind(c, name="sleep")
   end function sleep
