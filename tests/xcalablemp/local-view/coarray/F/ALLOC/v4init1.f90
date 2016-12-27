6ffdb111d430ded7d2ff00b7fdcff7945ec7e27f
Akio Wada <akio.wada@riken.jp>
2016-12-27 13:19:03 +0900

    modified tp for _XMP_FJRDMA where MALLOC_UNIT is 8.

--- tests/xcalablemp/local-view/coarray/F/ALLOC/v4init1.f90
+++ tests/xcalablemp/local-view/coarray/F/ALLOC/v4init1.f90
@@ -10,10 +10,15 @@ end subroutine readparam
 program main
   call readparam
   nbytes = xmpf_coarray_allocated_bytes()
-  nestim = 2*2*4+12
+  if (xmpf_coarray_uses_fjrdma()) then
+    nestim = 2*2*4+16
+  else
+    nestim = 2*2*4+12
+  endif
   if (nbytes == nestim) then
      write(*,*) "OK",nbytes,nestim
   else
      write(*,*) "NG",nbytes,nestim
+     call exit(1)
   endif
 end program main
