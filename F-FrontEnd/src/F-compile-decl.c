308a5c22cc3b560227e39c416f47e253a99886ae
shingo suzuki <shingo-s@sra.co.jp>
2017-02-15 16:53:13 +0900

    Fix for public/private in the derived-type declaration (#99)
    * Add tests
    
    * Fix bugs
    
    * Removed unused code

--- F-FrontEnd/src/F-compile-decl.c
+++ F-FrontEnd/src/F-compile-decl.c
@@ -2024,10 +2024,6 @@ declare_type_attributes(ID id, TYPE_DESC tp, expr attributes,
             TYPE_UNSET_PUBLIC(tp);
             TYPE_UNSET_PROTECTED(tp);
             TYPE_SET_PRIVATE(tp);
-            if (CTL_TYPE(ctl_top) == CTL_STRUCT) {
-                TYPE_DESC struct_tp = CTL_STRUCT_TYPEDESC(ctl_top);
-                TYPE_SET_INTERNAL_PRIVATE(struct_tp);
-            }
             break;
         case F03_PROTECTED_SPEC:
             if (TYPE_IS_PUBLIC(tp)) {
@@ -3926,6 +3922,57 @@ compile_struct_decl_end()
 
     /* check for type bound generic */
     check_type_bound_generics(stp);
+
+    if (!TYPE_IS_INTERNAL_PRIVATE(stp)) {
+        /*
+         * To transform the fortran code.
+         *
+         * TYPE t
+         *  INTEGER, PRIVATE :: a
+         *  INTEGER :: b
+         * END TYPE t
+         *
+         * will turn into
+         *
+         * TYPE t
+         *  PRIVATE
+         *  INTEGER :: a
+         *  INTEGER, PUBLIC :: b
+         * END TYPE t
+         *
+         */
+
+        ID mem;
+        int has_private_member = FALSE;
+
+        FOREACH_MEMBER(mem, stp) {
+            if (TYPE_IS_PRIVATE(ID_TYPE(mem))) {
+                has_private_member = TRUE;
+                break;
+            }
+        }
+
+        if (has_private_member) {
+            /*
+             * If the derived-type has a PRIVATE member,
+             * set is_internal_private to the derived-type.
+             */
+            TYPE_SET_INTERNAL_PRIVATE(stp);
+
+            /*
+             * Remove PRIVATE from PRIVATE members,
+             * and add PUBLIC to other members.
+             */
+            FOREACH_MEMBER(mem, stp) {
+                if (TYPE_IS_PRIVATE(ID_TYPE(mem))) {
+                    TYPE_UNSET_PRIVATE(ID_TYPE(mem));
+                } else {
+                    TYPE_SET_PUBLIC(ID_TYPE(mem));
+                }
+            }
+        }
+    }
+
 
     pop_ctl();
 }
