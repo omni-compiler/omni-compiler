5c3de64c8f4dc2afb18d19ce325a9f3a4fcff43c
Masahiro Nakao <masahiro.nakao@riken.jp>
2017-05-10 23:56:37 +0900

    Minor fix of script for make remote-tests

--- tests/xcalablemp/scripts/remote-tests.sh
+++ tests/xcalablemp/scripts/remote-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 ## Check arguments
 if test -z $1; then
@@ -13,16 +13,17 @@ fi
 PWD=`pwd`
 OMNI=`basename ${PWD}`
 PID=$$
-LOCAL_TMP_DIR=/tmp/omnicompiler-remote-test.${PID}
-REMOTE_TMP_DIR=${LOCAL_TMP_DIR}
+TMP_DIR=/tmp/omni-remote-tests-dir.${PID}
+CMD_FILE=${TMP_DIR}/run.sh
+STOP_FILE=${TMP_DIR}/stop.sh
 
-GASNET_OPENMPI_BASE_DIR=${REMOTE_TMP_DIR}/gasnet-openmpi
+GASNET_OPENMPI_BASE_DIR=${TMP_DIR}/gasnet-openmpi
 GASNET_OPENMPI_INSTALL_DIR=${GASNET_OPENMPI_BASE_DIR}/work
-GASNET_MPICH_BASE_DIR=${REMOTE_TMP_DIR}/gasnet-mpich
+GASNET_MPICH_BASE_DIR=${TMP_DIR}/gasnet-mpich
 GASNET_MPICH_INSTALL_DIR=${GASNET_MPICH_BASE_DIR}/work
-OPENMPI_BASE_DIR=${REMOTE_TMP_DIR}/openmpi
+OPENMPI_BASE_DIR=${TMP_DIR}/openmpi
 OPENMPI_INSTALL_DIR=${OPENMPI_BASE_DIR}/work
-MPICH_BASE_DIR=${REMOTE_TMP_DIR}/mpich
+MPICH_BASE_DIR=${TMP_DIR}/mpich
 MPICH_INSTALL_DIR=${MPICH_BASE_DIR}/work
 
 ARCHIVE=archive.tar.bz2
@@ -36,54 +37,43 @@ MPICH_PATH=/opt/mpich
 function clean_files()
 {
     echo "Clean temporal files ... "
-    rm -rf ${LOCAL_TMP_DIR}
-    ssh ${REMOTE_HOST} "rm -rf ${REMOTE_TMP_DIR} 2> /dev/null"
+    rm -rf ${TMP_DIR}
+    ssh ${REMOTE_HOST} "rm -rf ${TMP_DIR} 2> /dev/null"
 }
 
-function omni_exec()
-{
-    ${@+"$@"}
-    if test $? -ne 0; then
-	clean_files
-	exit 1
-     fi
-}
+trap 'ssh ${REMOTE_HOST} "[ -f ${STOP_FILE} ] && sh ${STOP_FILE}"; clean_files; exit 0' 1 2
 
 ## Create archive of omni-compiler
 echo "Compress ... "
-if test -d ${LOCAL_TMP_DIR}; then
-    echo "Error ! ${LOCAL_TMP_DIR} exist"
+if test -d ${TMP_DIR}; then
+    echo "Error ! ${TMP_DIR} exist"
     exit 1
-else
-    omni_exec mkdir -p ${LOCAL_TMP_DIR}
-    omni_exec cd ..
-    omni_exec omni_exec cp -a ${OMNI} ${LOCAL_TMP_DIR}/${OMNI}
-    omni_exec cd ${LOCAL_TMP_DIR}/${OMNI}
-    omni_exec make clean-tests > /dev/null
-    omni_exec make clean-tests-F2003 > /dev/null
-    omni_exec make clean > /dev/null
-    omni_exec cd ..
-    omni_exec tar cfj ${ARCHIVE} ${OMNI}
 fi
+
+mkdir -p ${TMP_DIR}
+cd ..
+cp -a ${OMNI} ${TMP_DIR}/${OMNI}
+cd ${TMP_DIR}/${OMNI}
+make clean-tests > /dev/null
+make clean-tests-F2003 > /dev/null
+make clean > /dev/null
+cd ..
+tar cfj ${ARCHIVE} ${OMNI}
 
 ## Transfer omni-compiler
 echo "Transfer archive ... "
-CMD="if test -d ${REMOTE_TMP_DIR}; then \
-         echo \"Error ${REMOTE_HOST}:${REMOTE_TMP_DIR} exist\"; exit 1;\
-     else\
-         mkdir -p ${REMOTE_TMP_DIR}; \
-     fi"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
-omni_exec scp ${LOCAL_TMP_DIR}/${ARCHIVE} ${REMOTE_HOST}:${REMOTE_TMP_DIR}
+CMD="mkdir -p ${TMP_DIR}"
+ssh ${REMOTE_HOST} ${CMD}
+scp ${TMP_DIR}/${ARCHIVE} ${REMOTE_HOST}:${TMP_DIR}
 
 ## Expand omni-compiler
 echo "Expand archive ..."
 CMD="mkdir -p ${GASNET_OPENMPI_BASE_DIR}; \
-     tar xfj ${REMOTE_TMP_DIR}/${ARCHIVE} -C ${GASNET_OPENMPI_BASE_DIR};\
+     tar xfj ${TMP_DIR}/${ARCHIVE} -C ${GASNET_OPENMPI_BASE_DIR};\
      cp -a ${GASNET_OPENMPI_BASE_DIR} ${GASNET_MPICH_BASE_DIR}; \
      cp -a ${GASNET_OPENMPI_BASE_DIR} ${OPENMPI_BASE_DIR}; \
      cp -a ${GASNET_OPENMPI_BASE_DIR} ${MPICH_BASE_DIR}"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
+ssh ${REMOTE_HOST} ${CMD}
 
 ## Output summury of compilers
 echo ""
@@ -93,63 +83,68 @@ echo "------------------------"
 CMD="LANG=C; gcc -v 2>&1 > /dev/null | tail -1 && \
      ls -l1 /opt/GASNet-openmpi | awk '{print \$11}' && \
      ls -l1 /opt/GASNet-mpich   | awk '{print \$11}'"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
+ssh ${REMOTE_HOST} ${CMD}
 
-## GASNet and openmpi
+cat << EOF > ${CMD_FILE}
+PID=\$$
+echo "PID is \${PID}"
+echo "cd .. ; rm -rf ${TMP_DIR}" > ${STOP_FILE}
+echo "kill \${PID}" >> ${STOP_FILE}
 echo ""
 echo "----------------------------------------------"
-echo "  Test omni compiler with GASNet and openmpi  "
+echo "  Test omni compiler with GASNet and OpenMPI  "
 echo "----------------------------------------------"
-CMD="export PATH=${OPENMPI_PATH}/bin:${GASNET_OPENMPI_INSTALL_DIR}/bin:$PATH && \
-     cd ${GASNET_OPENMPI_BASE_DIR}/${OMNI} && \
-     sh autogen.sh && \
-     ./configure --prefix=${GASNET_OPENMPI_INSTALL_DIR} --with-gasnet=${GASNET_OPENMPI_PATH} && make -j16 && make install && \
-     make clean-tests && make tests -j16 && make run-tests && \
-     make clean-tests-F2003 && make tests-F2003 -j16 && make run-tests-F2003"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
-
-## GASNet and mpich
+export PATH=${OPENMPI_PATH}/bin:${GASNET_OPENMPI_INSTALL_DIR}/bin:$PATH
+cd ${GASNET_OPENMPI_BASE_DIR}/${OMNI}
+sh autogen.sh
+./configure --prefix=${GASNET_OPENMPI_INSTALL_DIR} --with-gasnet=${GASNET_OPENMPI_PATH}
+make -j16; make install
+make clean-tests;       make tests -j16;       make run-tests
+make clean-tests-F2003; make tests-F2003 -j16; make run-tests-F2003
+exit 0
 echo ""
 echo "--------------------------------------------"
-echo "  Test omni compiler with GASNet and mpich  "
+echo "  Test omni compiler with GASNet and MPICH  "
 echo "--------------------------------------------"
-CMD="export PATH=${MPICH_PATH}/bin:${GASNET_MPICH_INSTALL_DIR}/bin:$PATH && \
-     cd ${GASNET_MPICH_BASE_DIR}/${OMNI} && \
-     sh autogen.sh && \
-     ./configure --prefix=${GASNET_MPICH_INSTALL_DIR} --with-gasnet=${GASNET_MPICH_PATH} && make -j16 && make install && \
-     make clean-tests && make tests -j16 && make run-tests && \
-     make clean-tests-F2003 && make tests-F2003 -j16 && make run-tests-F2003"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
+export PATH=${MPICH_PATH}/bin:${GASNET_MPICH_INSTALL_DIR}/bin:$PATH
+cd ${GASNET_MPICH_BASE_DIR}/${OMNI}
+sh autogen.sh
+./configure --prefix=${GASNET_MPICH_INSTALL_DIR} --with-gasnet=${GASNET_MPICH_PATH}
+make -j16; make install
+make clean-tests;       make tests -j16;       make run-tests
+make clean-tests-F2003; make tests-F2003 -j16; make run-tests-F2003
 
 ## OpenMPI
 echo ""
 echo "-----------------------------------"
 echo "  Test omni compiler with OpenMPI  "
 echo "-----------------------------------"
-CMD="export PATH=${OPENMPI_PATH}/bin:${OPENMPI_INSTALL_DIR}/bin:$PATH && \
-     cd ${OPENMPI_BASE_DIR}/${OMNI} && \
-     sh autogen.sh && \
-     ./configure --prefix=${OPENMPI_INSTALL_DIR} && make -j16 && make install && \
-     make clean-tests && make tests -j16 && make run-tests && \
-     make clean-tests-F2003 && make tests-F2003 -j16 && make run-tests-F2003"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
+export PATH=${OPENMPI_PATH}/bin:${OPENMPI_INSTALL_DIR}/bin:$PATH
+cd ${OPENMPI_BASE_DIR}/${OMNI}
+sh autogen.sh
+./configure --prefix=${OPENMPI_INSTALL_DIR}
+make -j16; make install
+make clean-tests;       make tests -j16;       make run-tests
+make clean-tests-F2003; make tests-F2003 -j16; make run-tests-F2003
 
 ## MPICH
 echo ""
 echo "---------------------------------"
 echo "  Test omni compiler with MPICH  "
 echo "---------------------------------"
-CMD="export PATH=${MPICH_PATH}/bin:${MPICH_INSTALL_DIR}/bin:$PATH && \
-     cd ${MPICH_BASE_DIR}/${OMNI} && \
-     sh autogen.sh && \
-     ./configure --prefix=${MPICH_INSTALL_DIR} && make -j16 && make install && \
-     make clean-tests && make tests -j16 && make run-tests && \
-     make clean-tests-F2003 && make tests-F2003 -j16 && make run-tests-F2003"
-omni_exec ssh ${REMOTE_HOST} ${CMD}
+export PATH=${MPICH_PATH}/bin:${MPICH_INSTALL_DIR}/bin:$PATH
+cd ${MPICH_BASE_DIR}/${OMNI}
+sh autogen.sh
+./configure --prefix=${MPICH_INSTALL_DIR}
+make -j16; make install
+make clean-tests;       make tests -j16;       make run-tests
+make clean-tests-F2003; make tests-F2003 -j16; make run-tests-F2003
+EOF
 
-echo "------------------"
-echo "  PASS ALL TESTS  "
-echo "------------------"
+scp ${CMD_FILE} ${REMOTE_HOST}:${CMD_FILE}
+ssh ${REMOTE_HOST} sh ${CMD_FILE}
 
+# clean up
+ssh ${REMOTE_HOST} rm -rf ${TMP_DIR}
 clean_files
 exit 0
