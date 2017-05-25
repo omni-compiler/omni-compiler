78e5792e5c905092d4597d82a7802ea795e4aac3
shingo suzuki <shingo-s@sra.co.jp>
2017-05-25 14:36:11 +0900

    Fix testdata

--- F-FrontEnd/test/testdata/formatted_io_write_formatted.f90
+++ F-FrontEnd/test/testdata/formatted_io_write_formatted.f90
@@ -8,11 +8,11 @@ module mod
   end type node
 
 contains
-  subroutine mywrite(dtv, unit, iotype, vlist, iostat, iomsg)
+  subroutine mywrite(dtv, unit, iotype, v_list, iostat, iomsg)
     class(node), intent(in) :: dtv
     integer, intent(in) :: unit
     character (len=*), intent(in) :: iotype
-    integer, intent(in) :: vlist(:)
+    integer, intent(in) :: v_list(:)
     integer, intent(out) :: iostat
     character (len=*), intent(inout) :: iomsg
     write (unit, '(dt)', iostat=iostat) dtv%i
@@ -20,7 +20,7 @@ contains
 end module mod
 
 program test
-  use mode
+  use mod
   type(node) :: tt
   tt%i = 3
   tt%a = 2.0
