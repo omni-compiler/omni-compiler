0835488636db6cf480d03bb70923424cf3da8e64
Valentin Clement <clementval@gmail.com>
2017-08-08 15:29:59 +0200

    Add test data for issue #208

---
+++ F-FrontEnd/test/testdata/issue208.f90
@@ -0,0 +1,17 @@
+MODULE mod1
+
+IMPLICIT NONE
+
+TYPE typ1
+  INTEGER :: nb
+END TYPE typ1
+
+TYPE (typ1), PARAMETER :: values_ref = typ1(1)
+
+CONTAINS
+  SUBROUTINE sub1
+    IMPLICIT NONE
+    INTEGER :: array1(10)
+    array1(:) = values_ref%nb
+  END SUBROUTINE sub1
+END MODULE mod1
