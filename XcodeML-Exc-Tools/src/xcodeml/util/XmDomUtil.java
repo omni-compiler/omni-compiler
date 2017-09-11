7eff6526625ae798555b5315cd2f23163de9b899
shingo suzuki <shingo-s@sra.co.jp>
2017-09-11 11:29:33 +0900

    Fix build error
    Remove a duplicate function

--- XcodeML-Exc-Tools/src/xcodeml/util/XmDomUtil.java
+++ XcodeML-Exc-Tools/src/xcodeml/util/XmDomUtil.java
@@ -51,12 +51,6 @@ public class XmDomUtil {
         return n.getFirstChild().getNodeValue();
     }
 
-    public static boolean hasAttr(Node n, String name) {
-        if (n == null)
-            return false;
-        return n.getAttributes().getNamedItem(name) != null;
-    }
-
     public static String getAttr(Node n, String name) {
         if (n == null)
             return null;
@@ -68,6 +62,8 @@ public class XmDomUtil {
     }
 
     public static boolean hasAttr(Node n, String name) {
+        if (n == null)
+            return false;
         String attr = getAttr(n, name);
         return attr != null && !attr.isEmpty();
     }
