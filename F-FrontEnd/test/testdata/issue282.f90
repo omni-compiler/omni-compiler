0f980d2aef5b1dc3c602f8d46acb464a4e94808f
Valentin Clement <clementval@gmail.com>
2017-09-25 15:20:20 +0200

    Update test data

--- F-FrontEnd/test/testdata/issue282.f90
+++ F-FrontEnd/test/testdata/issue282.f90
@@ -1,9 +1,11 @@
 module mod1
   implicit none
   use mo_issue282
+
+  real(kind=wp), parameter :: c1 = 0._wp
 contains
   subroutine sub1()
     implicit none
-    real(kind=wp) :: gcc(4) = (/c0, 500._wp, c0, c0/)
+    real(kind=wp) :: gcc(4) = (/c0, 500._wp, c0, c1/)
   end subroutine sub1
 end module mod1
