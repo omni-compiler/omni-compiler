6f5f111d90672c8cfa4a86003eb5ff4c03d3b4be
Valentin Clement <clementval@gmail.com>
2017-10-03 16:39:15 +0100

    Enhance test data

--- F-FrontEnd/test/testdata/issue306.f90
+++ F-FrontEnd/test/testdata/issue306.f90
@@ -2,3 +2,7 @@ program prog1
   character(len=:), allocatable :: string1
   character(kind=4,len=:),allocatable :: string2
 end program prog1
+
+subroutine sub1(string3)
+  character(kind=4,len=*) :: string3
+end subroutine sub1
