c121bfee2028899d3cce8d3d49e06b5c74cd9259
Valentin Clement <clementval@gmail.com>
2017-10-03 16:06:12 +0100

    Enhance test data

--- F-FrontEnd/test/testdata/issue304.f90
+++ F-FrontEnd/test/testdata/issue304.f90
@@ -3,9 +3,14 @@ integer, parameter :: fld_size = 10
   
 contains
 
- real(kind=8) function fld_fr()
-   dimension :: fld_fr(fld_size)
+  function fct1()
+    real, dimension(10) :: fct1 
+  end function fct1
+
+
+  real(kind=8) function fld_fr()
+    dimension :: fld_fr(fld_size)
  
- end function fld_fr
+  end function fld_fr
 
 end module mod1
