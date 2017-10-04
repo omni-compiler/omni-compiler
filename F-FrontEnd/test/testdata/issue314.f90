5fff82ed88e77ab7a45f0f16e9341cd1721562bf
Valentin Clement <clementval@gmail.com>
2017-10-04 10:32:00 +0100

    Add test data

---
+++ F-FrontEnd/test/testdata/issue314.f90
@@ -0,0 +1,15 @@
+module abstract_m
+  type typ1
+  contains
+    procedure, private :: assign0
+    generic :: dummy => assign0
+    generic, public :: assign => assign0
+  end type
+  interface
+    subroutine assign0(this)
+      import typ1
+      class(typ1) ,intent(in) :: this
+    end subroutine
+  end interface
+end module
+
