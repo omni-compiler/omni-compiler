0550249542201a6ead0095d314dab0a1707c21b5
Masahiro Nakao <masahiro.nakao@riken.jp>
2017-12-17 16:45:03 +0900

    Support module to call xmp program on multiple nodes by a python program on a single process

--- interfaces/xmp.py
+++ interfaces/xmp.py
@@ -1,6 +1,54 @@
+_init_lib = None
+_libname = ""
+_procs   = 0
+
 def init_py(lib, comm):
-    fcomm = comm.py2f()
+    global _init_lib
+    fcomm   = comm.py2f()
+    _init_lib = lib
     lib.xmp_init_py(fcomm)
 
-def finalize_py(lib):
-    lib.xmp_finalize()
+def finalize_py():
+    _init_lib.xmp_finalize()
+
+def spawn(libname, procs):
+    global _libname, _procs
+    _libname = libname
+    _procs   = procs
+    
+def run(funcname, *args):
+    from mpi4py import MPI
+    import tempfile,os,sys
+
+    tmpf = tempfile.NamedTemporaryFile(delete=False, dir="./")
+    tmpf.write("from mpi4py import MPI\n")
+    tmpf.write("import numpy\n")
+    tmpf.write("from ctypes import *\n\n")
+    tmpf.write("comm = MPI.Comm.Get_parent()\n")
+    
+    for (i,a) in enumerate(args):
+        argname = "arg" + str(i)
+        tmpf.write(argname + " = numpy.zeros(" + str(a.size) + ")\n")
+        tmpf.write("comm.Bcast(" + argname + ", root=0)\n")
+        
+    tmpf.write("lib = CDLL(\"" + _libname + "\")\n")
+    tmpf.write("lib.xmp_init_py(comm.py2f())\n")
+
+    tmpf.write("lib." + funcname + "(")
+    for (i,a) in enumerate(args):
+        tmpf.write("arg" + str(i) + ".ctypes")
+        if(i != len(args)-1):
+            tmpf.write(",")
+        
+    tmpf.write(")\n")
+    
+    tmpf.write("lib.xmp_finalize()\n")
+    tmpf.write("comm.Disconnect()\n")
+    tmpf.close()
+    comm = MPI.COMM_SELF.Spawn(sys.executable, args=[tmpf.name], maxprocs=_procs)
+    for a in args:
+        comm.Bcast(a, root=MPI.ROOT)
+        
+    comm.Disconnect()
+    os.unlink(tmpf.name)
+   
