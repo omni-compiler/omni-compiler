318f28caea699b07b21215f83549811c7bec8c33
shingo suzuki <shingo-s@sra.co.jp>
2017-12-18 12:29:39 +0900

    Fix syntax

--- F-FrontEnd/test/testdata/specific_name_and_generic_name.f90
+++ F-FrontEnd/test/testdata/specific_name_and_generic_name.f90
@@ -11,15 +11,15 @@
         END FUNCTION g
       END MODULE m1
 
-      !MODULE m2
-      !  TYPE t
-      !    INTEGER :: v
-      !  END TYPE t
-      !  INTERFACE t
-      !    MODULE PROCEDURE f
-      !  END INTERFACE t
-      ! CONTAINS
-      !  FUNCTION f(a)
-      !    INTEGER :: f, a
-      !  END FUNCTION f
-      !END MODULE m2
+      MODULE m2
+        TYPE t
+          INTEGER :: v
+        END TYPE t
+        INTERFACE t
+          MODULE PROCEDURE f
+        END INTERFACE t
+       CONTAINS
+        FUNCTION f(a)
+          INTEGER :: f, a
+        END FUNCTION f
+      END MODULE m2
