55d140bda1c12342feb091663c1b12e347697363
Masahiro Nakao <masahiro.nakao@riken.jp>
2018-01-05 09:04:59 +0900

    Rename directory

--- tests/xcalablemp/others/C/python/MPMD/test_async.py
+++
@@ -1,7 +0,0 @@
-import xmp
-
-prog1 = xmp.Program("test.so", "hello_2")
-job   = prog1.spawn(4, ([1,2,3], [3,4,5]), async=True)
-print ("elapsed_time:{0}".format(job.elapsed_time()) + "[sec]")
-job.wait()
-print ("elapsed_time:{0}".format(job.elapsed_time()) + "[sec]")
