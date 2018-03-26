e6f2184f114cd8ff5892d0d43a211630b3238865
Valentin Clement <clementval@gmail.com>
2018-03-26 13:28:37 +0200

    Add test data

---
+++ F-FrontEnd/test/testdata/issue563.f90
@@ -0,0 +1,16 @@
+MODULE mod1
+
+  USE issue563_mod, ONLY: t_msg
+
+  IMPLICIT NONE
+  PRIVATE
+
+  ABSTRACT INTERFACE
+    FUNCTION handler(this, msg_in) RESULT(return_ptr)
+      IMPORT :: t_msg
+      CLASS(t_msg), INTENT(inout) :: this
+      CLASS(t_msg), INTENT(in)    :: msg_in
+      CLASS(t_msg), POINTER       :: return_ptr
+    END FUNCTION handler
+  END INTERFACE
+END MODULE mod1
