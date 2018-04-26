66b34e2e59b9768286e0c013814480a826a36253
Valentin Clement <clementval@gmail.com>
2018-04-26 18:44:58 +0200

    Flag imported id as public is current state is not private

--- F-FrontEnd/src/F-compile.c
+++ F-FrontEnd/src/F-compile.c
@@ -5758,6 +5758,10 @@ import_module_id(ID mid,
                 SYM_NAME(use_name));
     }
 
+    if(current_module_state != M_PRIVATE) {
+        TYPE_SET_PUBLIC(ID_TYPE(id)); // Imported id should be flagged public
+    }
+
     return;
 }
 
