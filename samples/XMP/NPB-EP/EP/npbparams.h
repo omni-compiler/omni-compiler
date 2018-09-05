397e0d2957fb1039231352cec8f3c57d1ae45699
Masahiro Nakao <masahiro.nakao@riken.jp>
2018-09-05 17:23:47 +0900

    NPB-EP

---
+++ samples/XMP/NPB-EP/EP/npbparams.h
@@ -0,0 +1,18 @@
+/* CLASS = B */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	CLASS	 'B'
+#define	M	30
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "04 Jul 2010"
+#define NPBVERSION "2.3"
+#define CS1 "cc"
+#define CS2 "cc"
+#define CS3 "(none)"
+#define CS4 "-I../common"
+#define CS5 "-O3 "
+#define CS6 "(none)"
+#define CS7 "randdp"
