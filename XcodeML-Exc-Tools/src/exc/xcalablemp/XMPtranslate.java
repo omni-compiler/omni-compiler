e21675035bf4d8ffde8c348eae36869ced4ba161
Masahiro Nakao <masahiro.nakao@riken.jp>
2018-11-21 11:06:12 +0900

    Fix #592

--- XcodeML-Exc-Tools/src/exc/xcalablemp/XMPtranslate.java
+++ XcodeML-Exc-Tools/src/exc/xcalablemp/XMPtranslate.java
@@ -108,22 +108,19 @@ public class XMPtranslate implements XobjectDefVisitor {
   
   // Create a new function xmpc_main() and copy main() to the new function
   private void create_new_main(FuncDefBlock fd) throws XMPexception {
-
     XobjectFile _env      = _globalDecl.getEnv();
-
     Ident mainId          = _globalDecl.findVarIdent("main");
     Xtype mainType        = ((FunctionType)mainId.Type()).getBaseRefType();
 
-    if (!XmOption.getMainName().equals("")){
+    if(!XmOption.getMainName().equals(""))
       mainId = _env.declGlobalIdent(XmOption.getMainName(), Xtype.Function(mainType));
-    }
 
     Xobject mainIdList    = fd.getDef().getFuncIdList();
     Xobject mainDecls     = fd.getDef().getFuncDecls();
     Xobject mainBody      = fd.getDef().getFuncBody();
     
     Ident xmpcInitAll     = _env.declExternIdent("xmp_init_all", Xtype.Function(Xtype.voidType));
-    Ident xmpcMain        = _env.declStaticIdent("xmpc_main", Xtype.Function(mainType));
+    Ident xmpcMain        = _env.declGlobalIdent("xmpc_main", Xtype.Function(mainType));
     Ident xmpcFinalizeAll = _env.declExternIdent("xmp_finalize_all", Xtype.Function(Xtype.voidType));
 
     _env.add(XobjectDef.Func(xmpcMain, mainIdList, mainDecls, mainBody));
