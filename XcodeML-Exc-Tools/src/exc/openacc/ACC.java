e452f7defd70fe6eb5759c996d174fd32af2378c
takashift <e145771@ie.u-ryukyu.ac.jp>
2019-01-22 16:57:34 +0900

    change ACC.java

--- XcodeML-Exc-Tools/src/exc/openacc/ACC.java
+++ XcodeML-Exc-Tools/src/exc/openacc/ACC.java
@@ -26,8 +26,8 @@ public class ACC {
   public static final String FIND_DATA_FUNC_NAME = "_ACC_find_data";
 
   public static int version = 20;
-  // public static Platform platform = Platform.CUDA;
   public static Platform platform = Platform.outputACC;
+  // public static Platform platform = Platform.CUDA;
   public static AccDevice device = AccDevice.NONE;
 
   public static void exitByError() {
