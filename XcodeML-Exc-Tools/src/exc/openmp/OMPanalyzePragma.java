774064c8650c164d68b9c9102f59b270d6630251
Yutaka WATANABE <mail@ytn86.net>
2019-09-01 23:52:18 +0900

    fix indent

--- XcodeML-Exc-Tools/src/exc/openmp/OMPanalyzePragma.java
+++ XcodeML-Exc-Tools/src/exc/openmp/OMPanalyzePragma.java
@@ -266,8 +266,8 @@ public class OMPanalyzePragma
         case THREADPRIVATE:
             omp_env.declThreadPrivate(def.getDef().getDef(), pb, pb.getClauses());
             break;
-	case TARGET_ENTER_DATA:
-	    break;
+        case TARGET_ENTER_DATA:
+            break;
         default:
             OMP.fatal("unknown OpenMP pramga = " + p);
             break;
