3b6caa8f9bbd61b5f182b7f2654e9ea26fb4017c
takashift <e145771@ie.u-ryukyu.ac.jp>
2019-09-17 15:09:28 +0900

    add x.getName() for variable in AccHybridTranslator.java

--- XcodeML-Exc-Tools/src/exc/openacc/AccHybridTranslator.java
+++ XcodeML-Exc-Tools/src/exc/openacc/AccHybridTranslator.java
@@ -9,7 +9,7 @@ import exc.block.*;
 public class AccHybridTranslator implements XobjectDefVisitor {
     // private final ACCglobalDecl _globalDecl;
     // private final AccRewriter _rewrite;
-    
+
     public AccHybridTranslator(XobjectFile xobjFile) {
         if (!XmOption.isLanguageC()) {
             ACC.fatal("current version only supports C language.");
@@ -23,7 +23,16 @@ public class AccHybridTranslator implements XobjectDefVisitor {
     @Override
     public void doDef(XobjectDef def) {
 
-        System.out.println("Func name is " + def.getName());
+        String fname = def.getName();
+        System.out.println("Func name is " + fname);
+
+        XobjectIterator i = new topdownXobjectIterator(def.getFuncBody());
+        for(i.init(); !i.end(); i.next()){
+            Xobject x = i.getXobject();
+            if(x.isVariable() || x.isVarAddr())
+                System.out.println("Variable '"+x.getName()+"' is referenced from Function '"+fname+"'");
+        }
+
     //     if (def.isFuncDef()) {
     //         FuncDefBlock fd = new FuncDefBlock(def);
     //         FunctionBlock fb = fd.getBlock();
@@ -36,127 +45,130 @@ public class AccHybridTranslator implements XobjectDefVisitor {
     }
 
     // private void doFuncDef(FunctionBlock fb){
-    //     _rewrite.doFuncDef(fb);
-    //     ACC.exitByError();
+    // _rewrite.doFuncDef(fb);
+    // ACC.exitByError();
     // }
-    
+
     // private void doNonFuncDef(Xobject x){
-    //     _rewrite.doNonFuncDef(x);
-    //     ACC.exitByError();
+    // _rewrite.doNonFuncDef(x);
+    // ACC.exitByError();
     // }
-      
+
     // AccProcessor から
     // private void doFuncDef(FunctionBlock fb) {
-    //     BlockIterator blockIterator;
-    //     // if (_isTopdown) {
-    //     blockIterator = new topdownBlockIterator(fb);
-    //     // } else {
-    //     // blockIterator = new bottomupBlockIterator(fb);
-    //     // }
+    // BlockIterator blockIterator;
+    // // if (_isTopdown) {
+    // blockIterator = new topdownBlockIterator(fb);
+    // // } else {
+    // // blockIterator = new bottomupBlockIterator(fb);
+    // // }
 
-    //     for (blockIterator.init(); !blockIterator.end(); blockIterator.next()) {
-    //         Block b = blockIterator.getBlock();
-    //         switch (b.Opcode()) {
-    //         case ACC_PRAGMA:
-    //             try {
-    //                 doLocalAccPragma((PragmaBlock) b);
-    //             } catch (ACCexception e) {
-    //                 ACC.error(b.getLineNo(), e.getMessage());
-    //             }
-    //             break;
-    //         case PRAGMA_LINE:
-    //             if (_warnUnknownPragma) {
-    //                 ACC.warning(b.getLineNo(), "unknown pragma : " + b);
-    //             }
-    //             break;
-    //         default:
-    //         }
-    //     }
-    //     ACC.exitByError();
+    // for (blockIterator.init(); !blockIterator.end(); blockIterator.next()) {
+    // Block b = blockIterator.getBlock();
+    // switch (b.Opcode()) {
+    // case ACC_PRAGMA:
+    // try {
+    // doLocalAccPragma((PragmaBlock) b);
+    // } catch (ACCexception e) {
+    // ACC.error(b.getLineNo(), e.getMessage());
+    // }
+    // break;
+    // case PRAGMA_LINE:
+    // if (_warnUnknownPragma) {
+    // ACC.warning(b.getLineNo(), "unknown pragma : " + b);
+    // }
+    // break;
+    // default:
+    // }
+    // }
+    // ACC.exitByError();
     // }
 
     // // AccProcessor から
     // private void doNonFuncDef(Xobject x) {
-    //     switch (x.Opcode()) {
-    //     case ACC_PRAGMA:
-    //         try {
-    //             doGlobalAccPragma(x);
-    //         } catch (ACCexception e) {
-    //             ACC.error(x.getLineNo(), e.getMessage());
-    //         }
-    //         break;
-    //     case PRAGMA_LINE:
-    //         if (_warnUnknownPragma) {
-    //             ACC.warning(x.getLineNo(), "unknown pragma : " + x);
-    //         }
-    //         break;
-    //     default:
-    //     }
-    //     ACC.exitByError();
+    // switch (x.Opcode()) {
+    // case ACC_PRAGMA:
+    // try {
+    // doGlobalAccPragma(x);
+    // } catch (ACCexception e) {
+    // ACC.error(x.getLineNo(), e.getMessage());
+    // }
+    // break;
+    // case PRAGMA_LINE:
+    // if (_warnUnknownPragma) {
+    // ACC.warning(x.getLineNo(), "unknown pragma : " + x);
+    // }
+    // break;
+    // default:
+    // }
+    // ACC.exitByError();
     // }
 
     // void doLocalAccPragma(PragmaBlock pb) throws ACCexception {
-    //     doAccPragma(pb);
+    // doAccPragma(pb);
     // }
 
     // void doGlobalAccPragma(Xobject def) throws ACCexception {
-    //     doAccPragma(def);
+    // doAccPragma(def);
     // }
 
     // void doAccPragma(PropObject po) throws ACCexception {
-    //     Object obj = po.getProp(AccDirective.prop);
-    //     if (obj == null)
-    //         return;
-    //     AccDirective dire = (AccDirective) obj;
-    //     dire.rewrite();
+    // Object obj = po.getProp(AccDirective.prop);
+    // if (obj == null)
+    // return;
+    // AccDirective dire = (AccDirective) obj;
+    // dire.rewrite();
     // }
 
     // void rewrite() throws ACCexception {
-    //     if (isDisabled()) {
-    //         _pb.replace(Bcons.COMPOUND(_pb.getBody()));
-    //         return;
-    //     }
+    // if (isDisabled()) {
+    // _pb.replace(Bcons.COMPOUND(_pb.getBody()));
+    // return;
+    // }
 
-    //     // build
-    //     BlockList beginBody = Bcons.emptyBody();
-    //     for (Block b : initBlockList)
-    //         beginBody.add(b);
-    //     for (Block b : copyinBlockList)
-    //         beginBody.add(b);
-    //     BlockList endBody = Bcons.emptyBody();
-    //     for (Block b : copyoutBlockList)
-    //         endBody.add(b);
-    //     for (Block b : finalizeBlockList)
-    //         endBody.add(b);
+    // // build
+    // BlockList beginBody = Bcons.emptyBody();
+    // for (Block b : initBlockList)
+    // beginBody.add(b);
+    // for (Block b : copyinBlockList)
+    // beginBody.add(b);
+    // BlockList endBody = Bcons.emptyBody();
+    // for (Block b : copyoutBlockList)
+    // endBody.add(b);
+    // for (Block b : finalizeBlockList)
+    // endBody.add(b);
 
-    //     Block beginBlock = Bcons.COMPOUND(beginBody);
-    //     Block endBlock = Bcons.COMPOUND(endBody);
+    // Block beginBlock = Bcons.COMPOUND(beginBody);
+    // Block endBlock = Bcons.COMPOUND(endBody);
 
-    //     BlockList kernelsBody = Bcons.emptyBody();
-    //     for (Block b : _kernelBlocks) {
-    //         kernelsBody.add(b);
-    //     }
-    //     Block kernelsBlock = Bcons.COMPOUND(kernelsBody);
+    // BlockList kernelsBody = Bcons.emptyBody();
+    // for (Block b : _kernelBlocks) {
+    // kernelsBody.add(b);
+    // }
+    // Block kernelsBlock = Bcons.COMPOUND(kernelsBody);
 
-    //     BlockList resultBody = Bcons.emptyBody();
-    //     for (Xobject x : idList) {
-    //         resultBody.addIdent((Ident) x);
-    //     }
+    // BlockList resultBody = Bcons.emptyBody();
+    // for (Xobject x : idList) {
+    // resultBody.addIdent((Ident) x);
+    // }
 
-    //     Xobject ifExpr = _info.getIntExpr(ACCpragma.IF);
-    //     boolean isEnabled = (ifExpr == null || (ifExpr.isIntConstant() && !ifExpr.isZeroConstant()));
-    //     if (isEnabled) {
-    //         resultBody.add(beginBlock);
-    //         resultBody.add(kernelsBlock);
-    //         resultBody.add(endBlock);
-    //     } else {
-    //         Ident condId = resultBody.declLocalIdent("_ACC_DATA_IF_COND", Xtype.charType, StorageClass.AUTO, ifExpr);
-    //         resultBody.add(Bcons.IF(condId.Ref(), beginBlock, null));
-    //         resultBody.add(Bcons.IF(condId.Ref(), kernelsBlock, Bcons.COMPOUND(_pb.getBody())));
-    //         resultBody.add(Bcons.IF(condId.Ref(), endBlock, null));
-    //     }
+    // Xobject ifExpr = _info.getIntExpr(ACCpragma.IF);
+    // boolean isEnabled = (ifExpr == null || (ifExpr.isIntConstant() &&
+    // !ifExpr.isZeroConstant()));
+    // if (isEnabled) {
+    // resultBody.add(beginBlock);
+    // resultBody.add(kernelsBlock);
+    // resultBody.add(endBlock);
+    // } else {
+    // Ident condId = resultBody.declLocalIdent("_ACC_DATA_IF_COND", Xtype.charType,
+    // StorageClass.AUTO, ifExpr);
+    // resultBody.add(Bcons.IF(condId.Ref(), beginBlock, null));
+    // resultBody.add(Bcons.IF(condId.Ref(), kernelsBlock,
+    // Bcons.COMPOUND(_pb.getBody())));
+    // resultBody.add(Bcons.IF(condId.Ref(), endBlock, null));
+    // }
 
-    //     _pb.replace(Bcons.COMPOUND(resultBody));
+    // _pb.replace(Bcons.COMPOUND(resultBody));
     // }
 
     public void finish() {
