029fda62e82485444f4d6ef27b9d83ffbcd0d535
ShunIto <ito.shun@fujitsu.com>
2020-02-25 15:02:46 +0900

    move to function. xmp_coarray_fjrdma.c -> xmp_coarray_utils.c 	modified:   xmp_coarray_fjrdma.c 	modified:   xmp_coarray_utils.c

--- libxmp/src/xmp_coarray_fjrdma.c
+++ libxmp/src/xmp_coarray_fjrdma.c
@@ -38,6 +38,10 @@ extern int _XMP_flag_put_nb_rr_i;
 #define _XMP_COARRAY_FLAG_NIC_TMP_i3 (FJMPI_RDMA_LOCAL_NIC3 | FJMPI_RDMA_REMOTE_NIC3 | FJMPI_RDMA_IMMEDIATE_RETURN)
 /** End these variables are temporral **/ 
 
+#if 0
+// move to : xmp_coarray_utils.c
+// >>>
+
 /******************************************************************/
 /* DESCRIPTION : Set addresses                                    */
 /* ARGUMENT    : [OUT] *addrs     : Addresses                     */
@@ -1262,6 +1266,11 @@ static size_t _XMP_calc_stride(const _XMP_array_section_t *array_info, const int
  end:
   return stride[1] - stride[0];
 }
+
+// <<<
+// move to : xmp_coarray_utils.c
+#endif // #if 0
+
 /**
    Execute sync_memory for put operation
  */
