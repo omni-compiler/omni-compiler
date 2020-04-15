fe445bc65c66c66fe4a2436b46e331347d686ee4
Masahiro Nakao <masahiro.nakao@riken.jp>
2020-04-15 20:55:59 +0900

    minor fix for Fugaku

--- tests/XMP/local-view/coarray/F/find_exec_fc.sh
+++ tests/XMP/local-view/coarray/F/find_exec_fc.sh
@@ -1,4 +1,1 @@
-awk '
-  $1 ~ /OM_EXEC_F_COMPILER=.mpifrtpx./  { print "mpifrtpx" }                               
-  $1 ~ /OM_EXEC_F_COMPILER=.mpif90./    { print "mpif90" }                                 
-' ../../../../../config.log
+grep OM_EXEC_F_COMPILER= ../../../../../config.log | awk -F= '{print $2}' | xargs basename
